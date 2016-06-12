de2324e65f153b32375d69cab9c857fb2ae65cbd
gaoxu1@UTORONTO.CA <gaoxu1@UTORONTO.CA@fb2d0955-5374-4267-af28-b32595e4e7c6>
2016-06-12 22:06:28 +0000

    "formatted to follow google java format and added comments"

--- Assignment2/src/a2/Exit.java
+++ Assignment2/src/a2/Exit.java
@@ -1,7 +1,8 @@
 package a2;
 
 public class Exit {
-	public void exit(){
+	public void exit() {
+		// method to exit consoles
 		System.exit(0);
 	}
 
---
+++ Assignment2/src/a2/Output.java
@@ -0,0 +1,11 @@
+package a2;
+
+public class Output {
+	public void printMessage(String message){
+		if me		
+	}
+
+	public void logMessage(String message) {
+
+	}
+}
