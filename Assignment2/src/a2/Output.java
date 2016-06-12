d3194f3173ccccaa7658d0ed128c6a9bb320a432
gaoxu1@UTORONTO.CA <gaoxu1@UTORONTO.CA@fb2d0955-5374-4267-af28-b32595e4e7c6>
2016-06-12 22:12:13 +0000

    "updated output class with error message"

--- Assignment2/src/a2/Output.java
+++ Assignment2/src/a2/Output.java
@@ -2,7 +2,11 @@ package a2;
 
 public class Output {
 	public void printMessage(String message){
-		if me		
+		if (message == "error"){
+				System.out.println("That was not a recognized command");
+				
+		}
+			
 	}
 
 	public void logMessage(String message) {
