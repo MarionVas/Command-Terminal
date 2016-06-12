1af3bf1d2f2b0d6762c6491b93efb40e105609ef
vasant42 <MarionVas@users.noreply.github.com>
2016-06-12 21:18:15 +0000

    Created the initial Folder class; untested as of yet; subject to change

---
+++ a2/Folder.java
@@ -0,0 +1,29 @@
+package a2;
+
+import java.util.*;
+public class Folder {
+  private String dirName;
+  private String dirPath;
+  private Vector children;
+  
+  public Folder(String name, String path){
+    this.dirName = name;
+    this.dirPath = path;
+    this.children = new Vector();
+  }
+  public String getName(){
+    return this.dirName;
+  }
+  public String getPath(){
+    return this.dirPath;
+  }
+  public void addChildren(Folder child){
+    children.addElement(child);
+  }
+  public void addChildren(File child){
+    children.addElement(child);
+  }
+  public Object getChildren(int index){
+    return children.get(index);
+  }
+}
