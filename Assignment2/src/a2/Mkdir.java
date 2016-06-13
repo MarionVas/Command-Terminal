aec5c2e7675aab2b6b4ece0bec34f6466e0aa5a1
vasant42@UTORONTO.CA <vasant42@UTORONTO.CA@fb2d0955-5374-4267-af28-b32595e4e7c6>
2016-06-13 05:23:51 +0000

    Previous version had a bug where it would attempt to add children to the first folder(even though there weren't any); this problem has been fixed; To do: make mkdir work with multiple file creations

--- Assignment2/src/a2/Mkdir.java
+++ Assignment2/src/a2/Mkdir.java
@@ -15,22 +15,28 @@ public class Mkdir {
   public void create(String name) {
     if (name.startsWith("/")) {
       Vector<String> fullPaths = Manager.getFullPaths();
-      if (fullPaths.contains(name)) {
+      String parentPath = name.substring(0, name.lastIndexOf("/"));
+      // System.out.println(parentPath + "test1");
+      if (fullPaths.contains(parentPath)) {
+        // System.out.println(name + "test1");
         int currDirIndex = name.lastIndexOf("/");
         String currDir = name.substring(currDirIndex + 1, name.length());
+        System.out.println(currDir + "asdasd");
         Folder newFolder = new Folder(currDir, name);
         Manager.add(newFolder);
-        String parentPath = name.substring(0, name.lastIndexOf("/"));
-        Folder parentFolder = (Folder) Manager.getObject(parentPath);
-        parentFolder.addChildren(newFolder);
+        // parentPath = name.substring(0, name.lastIndexOf("/"));
+        if (Manager.getCurrPath().split("/", -1).length < 2) {
+          Folder parentFolder = (Folder) Manager.getObject(parentPath);
+          parentFolder.addChildren(newFolder);
+        }
         /*
          * int nameIndex = parentPath.lastIndexOf("/"); String parentName =
          * parentPath.substring(nameIndex, parentPath.length()); for (int i = 0;
          * i < Manager.getFullPaths().size(); i++){ Folder parentFolder =
          * (Folder) Manager.getObject(i); if (parentName ==
          * parentFolder.getName()){ parentFolder.addChildren(newFolder); } }
-         */
-        // System.out.println(currDir + " " + name);
+         */ // To be removed
+        // System.out.println(currDir + " " + name); //To be removed
       } else {
         // Return an error
       }
@@ -42,11 +48,24 @@ public class Mkdir {
         }
       }
       if (valid) {
-        String fullPath = Manager.getCurrPath() + "/" + name;
+        String fullPath = "";
+        if (Manager.getCurrPath().split("/", -1).length <= 2) {
+          System.out.println(Manager.getCurrPath().split("/", -1).length);
+          fullPath = Manager.getCurrPath() + name;
+        }
+
+        else {
+          fullPath = Manager.getCurrPath() + "/" + name;
+        }
         Folder newFolder = new Folder(name, fullPath);
         Manager.add(newFolder);
-        Folder parentFolder = (Folder) Manager.getObject(Manager.getCurrPath());
-        parentFolder.addChildren(newFolder);
+        // System.out.println("added2");
+        if (Manager.getCurrPath().split("/", -1).length < 2) {
+          Folder parentFolder =
+              (Folder) Manager.getObject(Manager.getCurrPath());
+          parentFolder.addChildren(newFolder);
+        }
+
         /*
          * int nameIndex = Manager.getCurrPath().lastIndexOf("/"); String
          * parentName = Manager.getCurrPath().substring(nameIndex + 1,
@@ -54,9 +73,9 @@ public class Mkdir {
          * Manager.getFullPaths().size(); i++){ Folder parentFolder = (Folder)
          * Manager.getObject(i); if (name == parentFolder.getName()){
          * parentFolder.addChildren(newFolder); } }
-         */
+         */ // To be removed
       } else {
-        // Return an error message
+        // Return an error message //To be removed
       }
     }
   }
