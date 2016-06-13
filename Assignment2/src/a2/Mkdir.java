ea12ee60c623cd1273069d31b651b0be913b3013
vasant42@UTORONTO.CA <vasant42@UTORONTO.CA@fb2d0955-5374-4267-af28-b32595e4e7c6>
2016-06-13 01:10:46 +0000

    Mkdir can now tell the difference between wrong arguments from right one; To be done: Create multiple folders at the same time, error message

--- Assignment2/src/a2/Mkdir.java
+++ Assignment2/src/a2/Mkdir.java
@@ -1,23 +1,45 @@
 package a2;
 
+import java.util.*;
+
 public class Mkdir {
   private FileSystem Manager;
-  
-  public Mkdir(FileSystem fileManager){
+  private String[] specialChar =
+      new String[] {"/", "!", "@", "$", "&", "#", "*", "(", ")", "?", ":", "[",
+          "]", "\"", "<", ">", "\'", "`", "\\", "|", "=", "{", "}", "/", ";"};
+
+  public Mkdir(FileSystem fileManager) {
     this.Manager = fileManager;
   }
-  public void create(String name){
-    if (name.startsWith("/")){
-      int currDirIndex = name.lastIndexOf("/");
-      String currDir = name.substring(currDirIndex+1, name.length());
-      Folder newFolder = new Folder(currDir, name);
-      Manager.add(newFolder);
-      System.out.println(currDir + "    " +  name);
-    }
-    else{
-      String fullPath = Manager.getCurrPath();
-      Folder newFolder = new Folder(name, fullPath);
-      Manager.add(newFolder);
+
+  public void create(String name) {
+    if (name.startsWith("/")) {
+      Vector<String> fullPaths = Manager.getFullPaths();
+      if (fullPaths.contains(name)) {
+        int currDirIndex = name.lastIndexOf("/");
+        String currDir = name.substring(currDirIndex + 1, name.length());
+        Folder newFolder = new Folder(currDir, name);
+        Manager.add(newFolder);
+        System.out.println(currDir + "    " + name);
+      } else {
+        // Return an error
+      }
+    } 
+    else {
+      boolean valid = true;
+      for (int i = 0; i < specialChar.length; i++){
+        if (name.contains(specialChar[i])){
+          valid = false;
+        }
+      }
+      if (valid){
+        String fullPath = Manager.getCurrPath();
+        Folder newFolder = new Folder(name, fullPath);
+        Manager.add(newFolder);
+      }
+      else {
+        //Return an error message
+      }
     }
   }
 }
