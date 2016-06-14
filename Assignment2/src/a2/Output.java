277f3f603e3b41edde1fd1502844dfb8777ec378
gaoxu1@UTORONTO.CA <gaoxu1@UTORONTO.CA@fb2d0955-5374-4267-af28-b32595e4e7c6>
2016-06-14 02:07:01 +0000

    "updated output file to two basic errors"

--- Assignment2/src/a2/Output.java
+++ Assignment2/src/a2/Output.java
@@ -1,15 +1,24 @@
 package a2;
 
 public class Output {
-	public void printMessage(String message){
-		if (message == "error"){
-				System.out.println("That was not a recognized command");
-				
-		}
-			
+	public void printError() {
+		System.out.println("That was not a recognized command");
 	}
 
-	public void logMessage(String message) {
+	public void printPath(String path) {
+		System.out.println(path);
+	}
+
+	public void printPathError(String pathError) {
+		System.out.println("That was not a valid path");
+	}
+
+	public void printContents(String contents) {
+		
+	}
+
+	public void printHistory(String[] history) {
 
 	}
+
 }
