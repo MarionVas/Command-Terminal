2155f44ecab6e4c09a8cd2fb57810d39e7282b6d
vasant42 <MarionVas@users.noreply.github.com>
2016-06-16 21:30:58 +0000

    added a vector to be able to store the String version of a folders children

--- a2/Folder.java
+++ a2/Folder.java
@@ -5,6 +5,7 @@ public class Folder extends FileSystem{
   private String dirName;
   private String dirPath;
   private Vector children;
+  private Vector<String> childrenNames;
   
   public Folder(String name, String path){
     this.dirName = name;
@@ -18,13 +19,23 @@ public class Folder extends FileSystem{
     return this.dirPath;
   }
   public void addChildren(Folder child){
-    children.addElement(child);
+    this.children.addElement(child);
+    this.childrenNames.addElement(child.getName());
   }
   public void addChildren(File child){
     children.addElement(child);
   }
   public Object getChildren(int index){
+    if (children.size() <= index){
+      return null;
+    }
     return children.get(index);
+  }
+  public Object getChildrenName(int index){
+    if (childrenNames.size() <= index){
+      return null;
+    }
+    return childrenNames.get(index);
   }
   public String toString(){
     return dirName + dirPath;
