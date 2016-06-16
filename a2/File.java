65316adba9bd8bbaecd7cfe58cf986b35806b2b5
gaoxu1 <gaoxu1@UTORONTO.CA>
2016-06-16 22:08:54 +0000

    "updated File class to contain a string body"

--- a2/File.java
+++ a2/File.java
@@ -1,5 +1,15 @@
 package a2;
 
-public class File extends FileSystem{
+public class File extends FileSystem {
+
+	String body = "";
+
+	public void addToBody(String append) {
+		this.body = this.body + "\n" + append;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
 
 }
