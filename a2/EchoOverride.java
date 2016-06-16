4947a17a108036d1c1967e29b288bce0a7f30c86
gaoxu1 <gaoxu1@UTORONTO.CA>
2016-06-16 23:54:26 +0000

    "initialized EchoAppend class, planned out implementation"

---
+++ a2/EchoOverride.java
@@ -0,0 +1,15 @@
+package a2;
+
+public class EchoOverride {
+	private FileSystem fileSystem;
+
+	public EchoOverride(FileSystem fileSystem) {
+	    this.fileSystem = fileSystem;
+	  }
+
+	public void createNewFile(String fileName){
+		String [] arguments = fileName.split(">");
+		
+	}
+
+}
