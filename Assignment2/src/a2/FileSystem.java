61b3d2d848a1f7a98305d6536ad8365b9f17b238
vasant42@UTORONTO.CA <vasant42@UTORONTO.CA@fb2d0955-5374-4267-af28-b32595e4e7c6>
2016-06-19 07:52:44 +0000

    Added a new way of searching through the directories, similar to searching through a tree

--- Assignment2/src/a2/FileSystem.java
+++ Assignment2/src/a2/FileSystem.java
@@ -4,7 +4,7 @@ public class FileSystem {
   //Output collaboration
   //DirStack collaboration
   //Vector Coordinates; //Unessercsay
-  private Vector Manager;
+  private Vector<Folder> Manager;
   private Vector<String> fullPaths;
   private String currDir;
   private Folder empty; //To be removed
@@ -17,7 +17,7 @@ public class FileSystem {
     this.fullPaths.addElement("/");
     this.fullPaths.addElement("");
     //empty = new Folder("empty", "/");
-    this.Manager.addElement(empty); //To be removed
+    //this.Manager.addElement(empty); //To be removed
     currDir = "/";
     //this.currDepth = 0;
   }
@@ -31,39 +31,48 @@ public class FileSystem {
     //System.out.println("getting" + Manager.get(0));
     return Manager.get(index);
   }
-
-  public Object getObject(String name) {
+  public Object getObject(String name){
+    //Only accepts an absolute path
     Object result = null;
-    if (name.startsWith("/")) {
-      /*
-       * String parentName = name.substring(name.lastIndexOf("/") + 1,
-       * name.length());
-       */ //To be removed
-      for (int i = 0; i < Manager.size(); i++) {
-        if (Manager.get(i).getClass().equals(Folder.class)) {
-          if (((Folder) Manager.get(i)).getPath() == name) {
-            result = Manager.get(i);
-          }
+    if (name.startsWith("/")){
+      
+      for (int i = 0; i < Manager.size(); i++){
+        if (result == null){
+          result = getObjRecurs(name, "/" + ((Folder) Manager.get(i)).getName(),Manager.get(i));
         }
-        /*
-         * else{ if (((File) Manager.get(i)).getPath() == name) { result =
-         * Manager.get(i); } }
-         */ // To be taken out of comment latter
-
       }
-    } else {
-      for (int i = 0; i < Manager.size(); i++) {
-        if (Manager.get(i).getClass().equals(Folder.class)) {
-          if (((Folder) Manager.get(i)).getName() == name) {
-            result = Manager.get(i);
-          }
+      
+    }
+    else{
+      name = this.currDir + "/" + name;
+      for (int i = 0; i < Manager.size(); i++){
+        if (result == null){
+          result = getObjRecurs(name, "/" + ((Folder) Manager.get(i)).getName(),Manager.get(i));
         }
-        /*
-         * else{ if (((File) Manager.get(i)).getName() == name){ result =
-         * Manager.get(i); } }
-         */ // To be taken out of comment later
-
       }
+    }
+    return result;
+  }
+  public Object getObjRecurs(String name, String currName,Folder dirrOrFile){
+    /*System.out.println(((currName+dirrOrFile.getName()).equals(name)));
+    System.out.println((currName+dirrOrFile.getName() == (name)));
+    System.out.println(currName + "     " + dirrOrFile.getName() + "      " + name);*/ 
+    if (dirrOrFile == null){
+      Object result = null;
+    }
+    else if (currName.equals(name)){
+      return dirrOrFile;
+    }
+    Vector allChildren = dirrOrFile.getAllChildren();
+    Object result = null;
+    for (int i = 0; result == null && allChildren != null &&i < allChildren.size(); i++){
+      if (allChildren.get(i).getClass().equals(Folder.class)){
+        result = getObjRecurs(name, currName + "/" + ((Folder) allChildren.get(i)).getName(), (Folder)allChildren.get(i));
+      }
+      else{
+        //get File's name and check
+      }
+      
     }
     return result;
   }
@@ -74,12 +83,15 @@ public class FileSystem {
     //this.currDepth++;
     //System.out.println("added");
   }
-  public void add(File newFile){
+  /*public void add(File newFile){
     Manager.addElement(newFile);
     //fullPaths.add(newFile.getPath()); To be taken out of comment later
-  }
+  }*/
   public Vector<String> getFullPaths(){
     return this.fullPaths;
+  }
+  public void addFullPath(String path){
+    this.fullPaths.addElement(path);
   }
   public boolean checkValidPath(String path){
     return this.fullPaths.contains(path);
@@ -102,4 +114,9 @@ public class FileSystem {
   public void increaseDepth(){
     this.currDepth++;
   }*/ //to be removed
+ 
 }
+
+
+
+
