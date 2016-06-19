da17504e0623388d9e2919a99798a7b5bb4620b7
vasant42@UTORONTO.CA <vasant42@UTORONTO.CA@fb2d0955-5374-4267-af28-b32595e4e7c6>
2016-06-19 08:10:29 +0000

    added the relative path cases for a directory input

--- Assignment2/src/a2/LS.java
+++ Assignment2/src/a2/LS.java
@@ -11,9 +11,71 @@ public class LS {
       Folder currFolder = (Folder) Manager.getObject(Manager.getCurrPath());
       int i = 0;
       while (currFolder.getChildren(i) != null){
-        contents = contents + currFolder.getChildrenName(i);
+        contents = contents + "\n" + currFolder.getChildrenName(i);
       }
+    }
+    else if (arg.startsWith("/")){
+      if (Manager.checkValidPath(arg)){
+        Folder currFolder = (Folder) Manager.getObject(arg);
+        int i = 0;
+        while (currFolder.getChildren(i) != null){
+          contents = contents + "\n" + currFolder.getChildrenName(i);
+        }
+        contents = arg + ": " + contents + "\n";
+      }
+      else{
+        //Print error
+      }
+    }
+    else if (arg.startsWith("..")){
+      int lastIndex = 0;
+      int count = 0;
+
+      while(lastIndex != -1){
+
+          lastIndex = arg.indexOf("..",lastIndex);
+
+          if(lastIndex != -1){
+              count ++;
+              lastIndex += 2;
+          }
+      }
+      String path = "";
+      for (int i = 1; i < Manager.getCurrPath().split("/").length - count; i++){
+        path =  path + "/" +  Manager.getCurrPath().split("/")[i];
+      }
+      arg = path + arg;
+      
+      if (Manager.checkValidPath(arg)){
+        Folder currFolder = (Folder) Manager.getObject(arg);
+        int i = 0;
+        while (currFolder.getChildren(i) != null){
+          contents = contents + "\n" + currFolder.getChildrenName(i);
+        }
+        contents = arg + ": " + contents + "\n";
+      }
+      else{
+        //Print error
+      }
+    }
+    else if (arg.contains("/")){
+      arg = Manager.getCurrPath() + "/" + arg;
+      if (Manager.checkValidPath(arg)){
+        Folder currFolder = (Folder) Manager.getObject(arg);
+        int i = 0;
+        while (currFolder.getChildren(i) != null){
+          contents = contents + "\n" + currFolder.getChildrenName(i);
+        }
+        contents = arg + ": " + contents + "\n";
+      }
+      else{
+        //Print error
+      }
+    }
+    else {
+      
     }
     return contents;
   }
+  
 }
