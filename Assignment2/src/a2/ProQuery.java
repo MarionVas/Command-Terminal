4d220b2641d84dd7be828a7688ea7ff397bd376d
haqueya1@UTORONTO.CA <haqueya1@UTORONTO.CA@fb2d0955-5374-4267-af28-b32595e4e7c6>
2016-06-20 00:10:21 +0000

    Further completed ProQuery code. Need to discuss how to interact with other command classes.

--- Assignment2/src/a2/ProQuery.java
+++ Assignment2/src/a2/ProQuery.java
@@ -2,6 +2,40 @@ package a2;
 
 public class ProQuery {
 
+  public static void sortQuery(String entry, boolean validity) {
+    if (validity == false) {
+      // either print or send something to output about invalid command
+    } else {
+      if (entry.length() <= 4) {
+        if (entry == "exit") {
+          // call exit command
+        } else if (entry == "pwd") {
+          // call pwd command
+        } else if (entry == "ls") {
+          // call ls command
+        } else if (entry == "popd") {
+          // call popd command
+        }
+      } else {
+        if (entry.startsWith("cd ")) {
+          // call cd command
+        } else if (entry.startsWith("cat ")) {
+          // call cat command
+        } else if (entry.startsWith("echo ")) {
+          // call appropriate echo command
+        } else if (entry.startsWith("history ")) {
+          // call history command
+        } else if (entry.startsWith("ls ")) {
+          // call ls command with argument
+        } else if (entry.startsWith("mkdir ")) {
+          // call mkdir command
+        } else if (entry.startsWith("pushd ")) {
+          // call pushd command
+        }
+      }
+    }
+  }
+
   public static void main(String[] args) {
     // TODO Auto-generated method stub
 
