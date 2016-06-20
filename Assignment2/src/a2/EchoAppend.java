ba5e1dc27d11db34eeebd1bbb772cab1713f6538
gaoxu1@UTORONTO.CA <gaoxu1@UTORONTO.CA@fb2d0955-5374-4267-af28-b32595e4e7c6>
2016-06-20 07:06:40 +0000

    "completed EchoAppend class, still requires documentation"

--- Assignment2/src/a2/EchoAppend.java
+++ Assignment2/src/a2/EchoAppend.java
@@ -1,10 +1,18 @@
 package a2;
 
-public class EchoAppend extends EchoOverride {
+public class EchoAppend extends EchoOverwrite {
 
-	public EchoAppend(FileSystem fileSystem) {
-		super(fileSystem);
-	}
-	  
+  public EchoAppend(String parameter) {
+    super(parameter);
+  }
 
+  private String findFileName() {
+    String fileName = getParameter().substring(getIndexOfSymbol() + 2,
+        getParameter().length());
+    return fileName;
+  }
+
+  private void replace(File file, String body) {
+    file.addToBody(body);
+  }
 }
