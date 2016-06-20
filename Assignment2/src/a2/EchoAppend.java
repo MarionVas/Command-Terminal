36f1fce8e8f3bc24ed8c2f92fb2e78e67076a238
gaoxu1@UTORONTO.CA <gaoxu1@UTORONTO.CA@fb2d0955-5374-4267-af28-b32595e4e7c6>
2016-06-20 21:44:13 +0000

    "documented EchoAppend class and added man method"

--- Assignment2/src/a2/EchoAppend.java
+++ Assignment2/src/a2/EchoAppend.java
@@ -2,9 +2,19 @@ package a2;
 
 public class EchoAppend extends EchoOverwrite {
 
+  /**
+   * The constructor uses the constructor from EchoOverwrite
+   */
   public EchoAppend(String parameter) {
     super(parameter);
   }
+
+  /**
+   * This function will return the name of the outfile given the parameter given
+   * to the constructor
+   * 
+   * @return fileName - The name of the outfile
+   */
 
   private String findFileName() {
     String fileName = getParameter().substring(getIndexOfSymbol() + 2,
@@ -12,7 +22,24 @@ public class EchoAppend extends EchoOverwrite {
     return fileName;
   }
 
+  /**
+   * This function will add string provided in the parameter to the body of the
+   * outfile
+   */
+
   private void replace(File file, String body) {
     file.addToBody(body);
+  }
+  
+  /**
+   * This function return the instructions on how to use the command echo 
+   * STRING [>> OUTFILE].
+   * 
+   * @return a string telling users the how the command works
+   */
+
+  public String man() {
+    return "modifies the outfile such that it appends an extra string\n"
+        + "line “STRING” to the end of the file. i.e. echo STRING >> OUTFILE";
   }
 }
