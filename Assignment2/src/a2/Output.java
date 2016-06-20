995cf50581cb27d62d148717b047dd62eed31a43
gaoxu1@UTORONTO.CA <gaoxu1@UTORONTO.CA@fb2d0955-5374-4267-af28-b32595e4e7c6>
2016-06-20 21:35:36 +0000

    "completed documentation for the Output class"

--- Assignment2/src/a2/Output.java
+++ Assignment2/src/a2/Output.java
@@ -1,19 +1,61 @@
 package a2;
 
+import java.util.Vector;
+
 public class Output {
+
+  /**
+   * This function display an error message claiming the command is invalid, if
+   * an invalid command is given
+   * 
+   */
+
   public static void printError() {
     System.out.println("That was not a recognized command.");
   }
 
-  public void printPathError() {
+  /**
+   * This function display an error message claiming the path is invalid, if an
+   * invalid path is given
+   * 
+   */
+
+  public static void printPathError() {
     System.out.println("That was not a valid path.");
   }
 
-  public void printFileNameError() {
+  /**
+   * This function display an error message claiming the file name is invalid,
+   * if an invalid file name is given
+   * 
+   */
+
+  public static void printFileNameError() {
     System.out.println("That was not a valid file name.");
   }
 
-  public void printContents(String contents) {
+
+  /**
+   * This function display a given string
+   * 
+   */
+
+
+  public static void printString(String contents) {
     System.out.println(contents);
+  }
+
+
+  /**
+   * This function display the contents of a given string vector with each
+   * element on a new line
+   * 
+   */
+
+
+  public static void printContents(Vector<String> contents) {
+    for (String content : contents) {
+      System.out.println(contents);
+    }
   }
 }
