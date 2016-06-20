535a753ead1b4a67c8a6b29027f379da50cf8553
gaoxu1@UTORONTO.CA <gaoxu1@UTORONTO.CA@fb2d0955-5374-4267-af28-b32595e4e7c6>
2016-06-20 05:41:12 +0000

    "fixed minor bug in CD, added error statements"

--- Assignment2/src/a2/CD.java
+++ Assignment2/src/a2/CD.java
@@ -2,30 +2,27 @@ package a2;
 
 public class CD {
   private FileSystem fileSystem = new FileSystem();
+  private Output output = new Output();
 
   public void execute(String path) {
     String currPath = fileSystem.getCurrPath();
     if (path == "..") {
       if (currPath == "/") {
-        // print Error
+        output.printPathError();
       } else {
-        currPath = currPath.substring(0, currPath.lastIndexOf("/") + 1);
+        currPath = currPath.substring(0, currPath.lastIndexOf("/"));
         fileSystem.setFullPath(currPath);
-        String lastFolder =
-            currPath.substring(currPath.lastIndexOf("/"), currPath.length());
-        fileSystem.setCurrFolder((Folder) fileSystem.getObject(lastFolder));
+        fileSystem.setCurrFolder((Folder) fileSystem.getObject(currPath));
       }
     } else if (path == ".") {
       // do nothing
     } else {
-      boolean correctPath = fileSystem.relativePathChecker(path);
+      boolean correctPath = fileSystem.checkValidPath(path);
       if (!correctPath) {
-        // print Error
+        output.printPathError();
       } else {
         fileSystem.setFullPath(path);
-        String lastFolder =
-            path.substring(path.lastIndexOf("/"), path.length());
-        fileSystem.setCurrFolder((Folder) fileSystem.getObject(lastFolder));
+        fileSystem.setCurrFolder((Folder) fileSystem.getObject(path));
 
       }
     }
