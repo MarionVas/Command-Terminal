aa24f6f01498726969dd6d4ea77be4f701078c71
haqueya1@UTORONTO.CA <haqueya1@UTORONTO.CA@fb2d0955-5374-4267-af28-b32595e4e7c6>
2016-06-21 04:06:49 +0000

    Changing ProQuery implementation so that it falls in line with the design we learned in tutorial today with the use of a hashtable.

--- Assignment2/src/a2/ProQuery.java
+++ Assignment2/src/a2/ProQuery.java
@@ -1,44 +1,80 @@
 package a2;
 
+import java.util.Arrays;
+import java.util.Hashtable;
+
 public class ProQuery {
 
-  public static void sortQuery(String entry, boolean validity) {
-    if (validity == false) {
-      // either print or send something to output about invalid command
-    } else {
-      if (entry.length() <= 4) {
-        if (entry == "exit") {
-          // call exit command
-        } else if (entry == "pwd") {
-          // call pwd command
-        } else if (entry == "ls") {
-          // call ls command
-        } else if (entry == "popd") {
-          // call popd command
-        }
-      } else {
-        if (entry.startsWith("cd ")) {
-          // call cd command
-        } else if (entry.startsWith("cat ")) {
-          // call cat command
-        } else if (entry.startsWith("echo ")) {
-          // call appropriate echo command
-        } else if (entry.startsWith("history ")) {
-          // call history command
-        } else if (entry.startsWith("ls ")) {
-          // call ls command with argument
-        } else if (entry.startsWith("mkdir ")) {
-          // call mkdir command
-        } else if (entry.startsWith("pushd ")) {
-          // call pushd command
-        }
-      }
+  public static void sortQuery(String entry) {
+    
+    Hashtable<String, String> singleCommandKeys =
+        new Hashtable<String, String>();
+    
+    Hashtable<String, String> commandKeys =
+        new Hashtable<String, String>();
+
+    singleCommandKeys.put("ls", "LS");
+    singleCommandKeys.put("exit", "Exit");
+    singleCommandKeys.put("pwd", "PWD");
+    singleCommandKeys.put("history", "History");
+    singleCommandKeys.put("popd", "PushD");
+
+    commandKeys.put("cd", "CD");
+    commandKeys.put("cat", "Cat");
+    commandKeys.put("echo", "");
+    commandKeys.put("ls", "LS");
+    commandKeys.put("man", "Man");
+    commandKeys.put("mkdir", "Mkdir");
+    commandKeys.put("history", "History");
+    commandKeys.put("pushd", "PopD");
+    
+    while(entry.startsWith(" ")) {
+      entry = entry.substring(1);
     }
+    
+    String[] splitEntry = entry.split(" ");
+    System.out.println(Arrays.toString(splitEntry));
+    System.out.println(splitEntry[0]);
+    
+    if (splitEntry.length <= 1) {
+        String commandName = singleCommandKeys.get(splitEntry[0]);
+        System.out.println(commandName);
+        try {
+          Class commandClass = Class.forName(commandName);
+          Object commandInstance = commandClass.newInstance();
+          
+        } catch (ClassNotFoundException e) {
+          // TODO Auto-generated catch block
+          // e.printStackTrace();
+          System.out.println("Invalid command1");
+        } catch (InstantiationException e) {
+          // TODO Auto-generated catch block
+          // e.printStackTrace();
+          System.out.println("Invalid command2");
+        } catch (IllegalAccessException e) {
+          // TODO Auto-generated catch block
+          // e.printStackTrace();
+          System.out.println("Invalid command3");
+        }
+        
+    }
+    
   }
 
   public static void main(String[] args) {
     // TODO Auto-generated method stub
-
+    String teststring = "                    this is a test      ";
+    while(teststring.startsWith(" ")) {
+      teststring = teststring.substring(1);
+    }
+    /* String [] test = teststring.split(" ");
+    System.out.println(Arrays.toString(test));
+    System.out.println(test.length);
+    System.out.println(test[0].length());
+    System.out.println("this   is a test".substring(1));
+    System.out.println("              this is a test".startsWith(" ")); */
+    ProQuery.sortQuery("exit");
+    System.out.println("this   is a test".substring(1));
   }
 
 }
