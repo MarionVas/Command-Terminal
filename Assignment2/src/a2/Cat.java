1da60e09ac2f0880fc8dc6d1492ee27cb5aed147
gaoxu1@UTORONTO.CA <gaoxu1@UTORONTO.CA@fb2d0955-5374-4267-af28-b32595e4e7c6>
2016-06-21 07:05:53 +0000

    "completed cat command to display the contents of a file"

--- Assignment2/src/a2/Cat.java
+++ Assignment2/src/a2/Cat.java
@@ -1,5 +1,25 @@
 package a2;
 
-public class Cat implements CommandInterface{ //require execute() method
+public class Cat implements CommandInterface { // require execute() method
+  private FileSystem fileSystem;
+  private String[] fileNames;
+
+  public Cat (JFileSystem manager, String[] files){
+    this.fileSystem = manager;
+    this.fileNames = files;
+  }
+
+
+
+  public void execute(){
+    Folder currFolder = fileSystem.getCurrFolder();
+    for(String eachFile: this.fileNames){
+      File file = (File) currFolder.getFile(eachFile);
+      Output.printContents(file.getBody());
+      Output.printString("");
+      Output.printString("");
+      Output.printString("");
+    }
+  }
 
 }
