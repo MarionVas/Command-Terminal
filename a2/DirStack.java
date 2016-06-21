f76e1dccc673017c13836cb877eec171f6d56de0
shahid41 <shahid41@UTORONTO.CA>
2016-06-21 06:05:46 +0000

    Added so it takes in a file System - once again, this entire class is temporary until further implementation issues are resolved

--- a2/DirStack.java
+++ a2/DirStack.java
@@ -5,25 +5,27 @@ import java.util.Stack;
 public class DirStack 
 {
   public Stack<String> DirectoryStack;
+  private JFileSystem fileSystem;
 
   // Constructor for the DirStack
-  public DirStack()
+  public DirStack(JFileSystem fileSystem)
   {
     // make the directory stack
     DirectoryStack = new Stack<String>();
+    this.fileSystem = fileSystem;
   }
   
   public void pushD(String dir)
   {
     DirectoryStack.push(dir);
-    CD location = new CD(null, dir);
+    CD location = new CD(fileSystem, dir);
     location.execute();
   }
   
   public void popD()
   {
     String filePath = (String) DirectoryStack.pop();
-    CD location = new CD(null, filePath);
+    CD location = new CD(fileSystem, filePath);
     location.execute();
     
   }
