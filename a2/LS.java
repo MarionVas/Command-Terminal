7ec758448b10a778b1f1a4d4d958f0beaf463095
vasant42 <MarionVas@users.noreply.github.com>
2016-06-21 05:33:47 +0000

    Added the ability to accept multiple arguments and list files

--- a2/LS.java
+++ a2/LS.java
@@ -6,45 +6,26 @@ import java.util.Vector;
 
 public class LS implements CommandInterface{
   private FileSystem Manager;
-  public LS(FileSystem fileManager){
+  private String[] args;
+  private String arg = "";
+  public LS(FileSystem fileManager, String[] name){
     this.Manager = fileManager;
+    this.args = name;
   }
-  public String execute(String arg){
-    String contents = "";
-    if (arg == ""){
-      if (Manager.getCurrPath() != "/"){
-        Folder currFolder = (Folder) Manager.getObject(Manager.getCurrPath());
-        Vector childNames = currFolder.getAllChildren();
-        java.util.Collections.sort(childNames);
-        int i = 0;
-        while (currFolder.getChildren(i) != null){
-          contents = contents + "\n" + childNames.get(i);
-        }
-      }
-      else{
-        int index = 0;
-        Vector childNames = new Vector();
-        while (Manager.getObject(index) != null){
-          childNames.add(((Folder) Manager.getObject(index)).getName());
-        }
-        java.util.Collections.sort(childNames);
-        int i = 0;
-        while (childNames.get(i) != null){
-          contents = contents + "\n" + childNames.get(i);
-        }
-      }
-    }
-    else if (arg.startsWith("/")){
-      if (Manager.checkValidPath(arg)){
+  public void execute(){
+    for (int indexarg = 0 ; indexarg < this.args.length; indexarg++){
+      this.arg = args[indexarg];
+    
+      String contents = "";
+      if (arg == ""){
         if (Manager.getCurrPath() != "/"){
-          Folder currFolder = (Folder) Manager.getObject(arg);
+          Folder currFolder = (Folder) Manager.getObject(Manager.getCurrPath());
           Vector childNames = currFolder.getAllChildren();
           java.util.Collections.sort(childNames);
           int i = 0;
           while (currFolder.getChildren(i) != null){
             contents = contents + "\n" + childNames.get(i);
           }
-          contents = arg + ": " + contents + "\n";
         }
         else{
           int index = 0;
@@ -55,83 +36,127 @@ public class LS implements CommandInterface{
           java.util.Collections.sort(childNames);
           int i = 0;
           while (childNames.get(i) != null){
-            contents = contents + "\n" + childNames.get(i);
+            contents = contents + "      " + childNames.get(i);
+            contents = contents + "\n";
           }
-          contents = arg + ": " + contents + "\n";
         }
       }
-      else{
-        //Print error
-      }
-    }
-    else if (arg.startsWith("..")){
-      int lastIndex = 0;
-      int count = 0;
-
-      while(lastIndex != -1){
-
-          lastIndex = arg.indexOf("..",lastIndex);
-
-          if(lastIndex != -1){
-              count ++;
-              lastIndex += 2;
+      else if (arg.startsWith("/")){
+        if (Manager.checkValidPath(arg)){
+          if (Manager.getCurrPath() != "/"){
+            if (Manager.getObject(arg).getClass().equals(Folder.class)){
+              
+              Folder currFolder = (Folder) Manager.getObject(arg);
+              Vector childNames = currFolder.getAllChildren();
+              java.util.Collections.sort(childNames);
+              int i = 0;
+              while (currFolder.getChildren(i) != null){
+                contents = contents + "     " + childNames.get(i);
+              }
+              contents = arg + ": " + contents + "\n";
+            }
+            else {
+              contents = arg + "\n";
+            }
           }
-      }
-      String path = "";
-      for (int i = 1; i < Manager.getCurrPath().split("/").length - count; i++){
-        path =  path + "/" +  Manager.getCurrPath().split("/")[i];
-      }
-      arg = path + arg;
-      
-      if (Manager.checkValidPath(arg)){
-        if (Manager.getCurrPath() != "/"){
-          Folder currFolder = (Folder) Manager.getObject(arg);
-          Vector childNames = currFolder.getAllChildren();
-          java.util.Collections.sort(childNames);
-          int i = 0;
-          while (currFolder.getChildren(i) != null){
-            contents = contents + "\n" + childNames.get(i);
+          else{
+            int index = 0;
+            Vector childNames = new Vector();
+            while (Manager.getObject(index) != null){
+              childNames.add(((Folder) Manager.getObject(index)).getName());
+            }
+            java.util.Collections.sort(childNames);
+            int i = 0;
+            while (childNames.get(i) != null){
+              contents = contents + "    " + childNames.get(i);
+            }
+            contents = arg + ": " + contents + "\n";
           }
-          contents = arg + ": " + contents + "\n";
         }
         else{
-          int index = 0;
-          Vector childNames = new Vector();
-          while (Manager.getObject(index) != null){
-            childNames.add(((Folder) Manager.getObject(index)).getName());
-          }
-          java.util.Collections.sort(childNames);
-          int i = 0;
-          while (childNames.get(i) != null){
-            contents = contents + "\n" + childNames.get(i);
-          }
-          contents = arg + ": " + contents + "\n";
+          Output.printPathError();
         }
       }
-      else{
-        //Print error
-      }
-    }
-    else if (arg.contains("/")){
-      arg = Manager.getCurrPath() + "/" + arg;
-      if (Manager.checkValidPath(arg)){
-        Folder currFolder = (Folder) Manager.getObject(arg);
-        Vector childNames = currFolder.getAllChildren();
-        java.util.Collections.sort(childNames);
-        int i = 0;
-        while (currFolder.getChildren(i) != null){
-          contents = contents + "\n" + childNames.get(i);
+      else if (arg.startsWith("..")){
+        int lastIndex = 0;
+        int count = 0;
+  
+        while(lastIndex != -1){
+  
+            lastIndex = arg.indexOf("..",lastIndex);
+  
+            if(lastIndex != -1){
+                count ++;
+                lastIndex += 2;
+            }
         }
-        contents = arg + ": " + contents + "\n";
+        String path = "";
+        for (int i = 1; i < Manager.getCurrPath().split("/").length - count; i++){
+          path =  path + "/" +  Manager.getCurrPath().split("/")[i];
+        }
+        arg = path + arg;
+        
+        if (Manager.checkValidPath(arg)){
+          if (Manager.getCurrPath() != "/"){
+            if (Manager.getObject(arg).getClass().equals(Folder.class)){
+              Folder currFolder = (Folder) Manager.getObject(arg);
+              Vector childNames = currFolder.getAllChildren();
+              java.util.Collections.sort(childNames);
+              int i = 0;
+              while (currFolder.getChildren(i) != null){
+                contents = contents + "     " + childNames.get(i);
+              }
+              contents = arg + ": " + contents + "\n";
+            }
+            else {
+              contents = arg + "\n";
+            }
+          }
+          else{
+            int index = 0;
+            Vector childNames = new Vector();
+            while (Manager.getObject(index) != null){
+              childNames.add(((Folder) Manager.getObject(index)).getName());
+            }
+            java.util.Collections.sort(childNames);
+            int i = 0;
+            while (childNames.get(i) != null){
+              contents = contents + "      " + childNames.get(i);
+            }
+            contents = arg + ": " + contents + "\n";
+          }
+        }
+        else{
+          Output.printPathError();
+        }
       }
-      else{
-        //Print error
+      else if (arg.contains("/")){
+        arg = Manager.getCurrPath() + "/" + arg;
+        if (Manager.checkValidPath(arg)){
+            if (Manager.getObject(arg).getClass().equals(Folder.class)){
+              Folder currFolder = (Folder) Manager.getObject(arg);
+              Vector childNames = currFolder.getAllChildren();
+              java.util.Collections.sort(childNames);
+              int i = 0;
+              while (currFolder.getChildren(i) != null){
+                contents = contents + "     " + childNames.get(i);
+              }
+              contents = arg + ": " + contents + "\n";
+            }
+            else{
+              contents = arg + "\n";
+            }
+            
+        }
+        else{
+          Output.printPathError();
+        }
       }
+      else {
+        Output.printError();
+      }
+      Output.printString(contents);
     }
-    else {
-      //File case
-    }
-    return contents;
   }
   
 }
