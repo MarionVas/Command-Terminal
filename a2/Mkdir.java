36bf48eb9f8fdd2fe15abd8c5b411778aa1af40e
vasant42 <MarionVas@users.noreply.github.com>
2016-06-24 02:30:47 +0000

    Can now perfectly handle any ".." case; fixed a bug in the local path case

--- a2/Mkdir.java
+++ a2/Mkdir.java
@@ -15,64 +15,64 @@ public class Mkdir implements CommandInterface {
     this.names = arg;
 
   }
-  public void executeFullPath(String name){
+
+  public void executeFullPath(String name) {
     String parentPath = name.substring(0, name.lastIndexOf("/"));
     if (Manager.checkValidPath(parentPath)) {
       int currDirIndex = name.lastIndexOf("/");
       String currDir = name.substring(currDirIndex + 1, name.length());
       Folder newFolder = new Folder(currDir, name);
       Manager.addFullPath(name);
-      Folder parentFolder =
-          (Folder) Manager.getObject(parentPath);
+      Folder parentFolder = (Folder) Manager.getObject(parentPath);
       parentFolder.addChildren(newFolder);
     } else if (parentPath.equals("")) {
       int currDirIndex = name.lastIndexOf("/");
       String currDir = name.substring(currDirIndex + 1, name.length());
       Folder newFolder = new Folder(currDir, name);
       Manager.addFullPath(name);
-      Folder parentFolder =
-          (Folder) Manager.getObject("/");
+      Folder parentFolder = (Folder) Manager.getObject("/");
       parentFolder.addChildren(newFolder);
 
     } else {
       Output.printPathError();
     }
   }
-  public String removeDots(String name){
-    int lastIndex = 0;
-    int count = 0;
 
-    while (lastIndex != -1) {
+  public String removeDots(String name) {
+    int endIndex = 0;
+    int numOfOccurences = 0;
 
-      lastIndex = name.indexOf("..", lastIndex);
+    while (endIndex != -1) {
 
-      if (lastIndex != -1) {
-        count++;
-        lastIndex += 2;
+      endIndex = name.indexOf("..", endIndex);
+
+      if (endIndex != -1) {
+        numOfOccurences++;
+        endIndex += 2;
       }
     }
-    String path = "";
+    String path = Manager.getCurrPath();
     int indexDots = name.indexOf("/");
-    if (Manager.getCurrPath().split("/").length - count == 1) {
-      path = "/";
-      name = name.substring(indexDots + 1, name.length());
-
-    }
-    for (int i = 1; i < Manager.getCurrPath().split("/").length
-        - count; i++) {
-      path = path + "/" + Manager.getCurrPath().split("/")[i];
-      if (!name.startsWith("..")){
+    for (int i = 0; i < numOfOccurences; i++) {
+      if (!name.startsWith("..") && name.contains("..")) {
         path = path + "/" + name.substring(0, indexDots);
         i--;
+      } else {
+        try {
+          path = path.substring(0, path.lastIndexOf("/"));
+        } catch (Exception e) {
+          path = "//////////";
+          break;
+        }
       }
-      name = name.substring(indexDots+1, name.length());
+      name = name.substring(indexDots + 1, name.length());
       indexDots = name.indexOf("/");
 
     }
-    System.out.println(path + name);
-    return path + name;
+    return path + "/" + name;
   }
-  public void executeLocal(String name){
+
+  public void executeLocal(String name) {
     boolean valid = true;
     for (int i = 0; i < specialChar.length; i++) {
       if (name.contains(specialChar[i])) {
@@ -90,84 +90,54 @@ public class Mkdir implements CommandInterface {
       }
       Folder newFolder = new Folder(name, fullPath);
       Manager.addFullPath(fullPath);
-      Folder parentFolder =
-          (Folder) Manager.getObject(Manager.getCurrPath());
+      Folder parentFolder = (Folder) Manager.getObject(Manager.getCurrPath());
       parentFolder.addChildren(newFolder);
     } else {
       Output.printFileNameError();
     }
   }
+
   public void execute() {
     for (int index = 0; index < this.names.length; index++) {
       String name = this.names[index];
-      if (name.endsWith("/")){
-        name = name.substring(0, name.length()-1);
+      if (name.endsWith("/")) {
+        name = name.substring(0, name.length() - 1);
       }
       if (name.startsWith("/")) {
-        if (Manager.checkValidPath(name)){
+        if (Manager.checkValidPath(name)) {
           Output.printError();
-        }
-        else {
+        } else {
           this.executeFullPath(name);
         }
-      }
-      else if (name.startsWith("..")) { 
+      } else if (name.startsWith("..")) {
         name = this.removeDots(name);
-        if (Manager.checkValidPath(name)){
+        if (Manager.checkValidPath(name)) {
           Output.printError();
-        }
-        else {
+        } else {
           this.executeFullPath(name);
         }
       } else if (name.contains("/")) {
-        name = Manager.getCurrPath() + "/" + name;
-        if (Manager.checkValidPath(name)){
-          Output.printError();
+        if (Manager.getCurrPath().equals("/")){
+          name = "/" + name;
         }
         else {
+          name = Manager.getCurrPath() + "/" + name;
+        }
+        
+        if (Manager.checkValidPath(name)) {
+          Output.printError();
+        } else {
           this.executeFullPath(name);
         }
 
       } else {
-        if (Manager.checkValidPath(name)){
+        if (Manager.checkValidPath(name)) {
           Output.printError();
-        }
-        else {
+        } else {
           this.executeLocal(name);
         }
       }
     }
   }
- public static void main(String[] args) 
-  {
-    /*String s = "/eeee/qqqq/w";
-    String s1 = "wefedsd";
-    String s2 = "/eedd";
-    String s3 = "/";
-    System.out.println(s.split("/", -1)[0] + "   " + s1.split("/", -1)[0] + " " + s2.split("/", -1)[1] + " " + s3.split("/", -1)[2]);*/
-   JFileSystem man = new JFileSystem();;
-    Folder rootfold = new Folder("/", "/");
-    man.setRoot(rootfold);
-    String[] s = new String[1];
-    s[0] = "qawsed";
-    Mkdir mkdir = new Mkdir(man, s);
-    mkdir.execute();
-    System.out.println(((Folder) man.getObject("/qawsed")).getPath() + "  wew");
-    s[0] = "rsxq";
-    mkdir = new Mkdir(man, s);
-    mkdir.execute();
-    System.out.println(((Folder) man.getObject("/rsxq")).getPath() + "  AWR" );
-    s[0] = "/rsxq/wer";
-    mkdir = new Mkdir(man, s);
-    mkdir.execute();
-    man.setFullPath("/rsxq");
-    s[0] = "../wer";
-    mkdir = new Mkdir(man, s);
-    mkdir.execute();
-    //System.out.println(((Folder) man.getObject(2)).getPath() + "  wdw" );
-    Folder fold = (Folder) man.getObject("/wer");
-    System.out.println(fold.getName());
-    man.setFullPath("/a/b/c/d/q/e");
-    
-  }
+
 }
