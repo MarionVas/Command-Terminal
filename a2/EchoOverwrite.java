d1cb9415b1f585114562fd29f11ea7cb3d04ef9d
gaoxu1 <gaoxu1@UTORONTO.CA>
2016-06-24 01:51:57 +0000

    "corrected error that didn't allow inheritance as wanted"

--- a2/EchoOverwrite.java
+++ a2/EchoOverwrite.java
@@ -5,12 +5,10 @@ import java.util.Vector;
 public class EchoOverwrite implements CommandInterface {
   // collaboration with fileSystem
   private FileSystem fileSystem; // del equals
-  // initialize a variable to hold the index of "<"
+  // initialize a variable to hold the index of ">"
   private int indexOfSymbol;
   // initialize a variable to hold the parameter passed to the command
-  private String parameter;
-  // get the current working directory
-  private Folder currFolder = fileSystem.getCurrFolder();
+  private String[] parameter;
 
   /**
    * The constructor
@@ -19,9 +17,7 @@ public class EchoOverwrite implements CommandInterface {
   public EchoOverwrite(JFileSystem manager, String[] parameter) {
     // fileSystem = FileSystem.createFileSystemInstance();
     this.fileSystem = manager;
-    this.parameter = parameter[0];
-    // finds the index of the ">" symbol
-    this.indexOfSymbol = this.parameter.indexOf(">");
+    this.parameter = parameter;
   }
 
   /**
@@ -33,41 +29,45 @@ public class EchoOverwrite implements CommandInterface {
 
   public void execute() {
     // check if an outfile is specified
-    if (this.indexOfSymbol == -1) {
+    if (parameter.length == 1) {
       // if an outfile is not specified, display the string
-      Output.printString(parameter);
+      Output.printString(parameter[0]);
     } else {
-      // if an outfile is specified, get the string portion
-      String body = parameter.substring(0, indexOfSymbol - 1);
-      // find the name of the outfile
-      String fileName = findFileName();
-      // get the outfile
-      File file = currFolder.getFile(fileName);
-      // check if the file exists
-      if (file.equals(null)) {
-        // if the file does not exist display an error
-        File newFile = new File(fileName);
-        currFolder.addChildren(newFile);
-      } else {
-        // if the file exists, replace the body of the file with the string
-        replace(file, body);
-      }
+      File replaceFile = findFile();
+      replace(replaceFile, parameter[0]);
     }
   }
 
-  /**
-   * This function will return the name of the outfile given the parameter given
-   * to the constructor
+  public File findFile() {
+    // get the current working directory
+    Folder currFolder = fileSystem.getCurrFolder();
+    // find the name of the outfile
+    String fileName = parameter[2];
+    // get the outfile
+    File file = currFolder.getFile(fileName);
+    // check if the file exists
+    if (file == null) {
+      // if the file does not exist display an error
+      file = new File(fileName);
+      currFolder.addChildren(file);
+      // if the file exists, replace the body of the file with the string
+    }
+    return file;
+
+  }
+
+  /*
+   * /** This function will return the name of the outfile given the parameter
+   * given to the constructor
    * 
    * @return fileName - The name of the outfile
+   * 
+   * 
+   * private String findFileName() { // find the name of the outfile String
+   * fileName = this.parameter.substring(this.indexOfSymbol + 2,
+   * this.parameter.length()); return fileName; }
    */
 
-  private String findFileName() {
-    // find the name of the outfile
-    String fileName = this.parameter.substring(this.indexOfSymbol + 3,
-        this.parameter.length());
-    return fileName;
-  }
 
   /**
    * This function will replace the body of the file with the string provided in
@@ -78,16 +78,17 @@ public class EchoOverwrite implements CommandInterface {
     file.setBody(body);
   }
 
-  /**
-   * This function return the index of the symbol ">".
+  /*
+   * /** This function return the index of the symbol ">".
    * 
    * @return indexOfSymbol - The index of the symbol ">" provided in the
-   *         constructor
+   * constructor
+   * 
+   * 
+   * public int getIndexOfSymbol() { return indexOfSymbol; }
    */
 
-  public int getIndexOfSymbol() {
-    return indexOfSymbol;
-  }
+
 
   /**
    * This function return the parameter provided to the constructor
@@ -95,22 +96,22 @@ public class EchoOverwrite implements CommandInterface {
    * @return parameter - The parameter provided to the constructor
    */
 
-  public String getParameter() {
+  public String[] getParameter() {
     return parameter;
   }
 
   /**
-   * This function return the instructions on how to use the command echo 
-   * String [> OUTFILE].
+   * This function return the instructions on how to use the command echo String
+   * [> OUTFILE].
    * 
    * @return a string telling users the how the command works
    */
-  
-  public String man(){
+
+  public String man() {
     return "Print a string on the shell. If a “> OUTFILE” parameter is\n"
-        + "specified within the command, modify the file such that it only\n" 
+        + "specified within the command, modify the file such that it only\n"
         + "contains the given string argument “STRING”. If the file does \n"
-        + "not exist, create a new file with the given string argument as the\n" 
+        + "not exist, create a new file with the given string argument as the\n"
         + "body. i.e. echo STRING > OUTFILE";
   }
 
