e8c049673f034115ef1b95f830e1f71dc81c6c6a
gaoxu1 <gaoxu1@UTORONTO.CA>
2016-06-24 01:57:05 +0000

    "fixed missing case"

--- a2/Echo.java
+++ a2/Echo.java
@@ -3,19 +3,22 @@ package a2;
 public class Echo implements CommandInterface {
   private JFileSystem jFileSystem;
   private String[] echoParameters;
-  
+
   public Echo(JFileSystem jFileSystem, String[] echoParameters) {
     this.jFileSystem = jFileSystem;
     this.echoParameters = echoParameters;
-}
+  }
 
   public void execute() {
     if (echoParameters[1].equals(">")) {
-      EchoOverwrite overwriteFile = new EchoOverwrite(jFileSystem, echoParameters);
+      EchoOverwrite overwriteFile =
+          new EchoOverwrite(jFileSystem, echoParameters);
       overwriteFile.execute();
     } else if (echoParameters[1].equals(">>")) {
       EchoAppend appendFile = new EchoAppend(jFileSystem, echoParameters);
       appendFile.execute();
+    } else if (echoParameters.length == 1) {
+      Output.printString(echoParameters[0]);
     } else {
       Output.printError();
     }
