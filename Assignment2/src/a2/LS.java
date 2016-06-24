44ef230205942f069d0724791637694fa872fbd1
vasant42@UTORONTO.CA <vasant42@UTORONTO.CA@fb2d0955-5374-4267-af28-b32595e4e7c6>
2016-06-24 06:52:12 +0000

    Fully commented and formated; Work for all possible cases; All present bugs have been fixed; Separated into smaller helper methods

--- Assignment2/src/a2/LS.java
+++ Assignment2/src/a2/LS.java
@@ -4,210 +4,215 @@ import java.awt.Component;
 import java.awt.List;
 import java.util.Vector;
 
-public class LS implements CommandInterface{
+public class LS implements CommandInterface {
+  // The jFileSystem
   private FileSystem Manager;
+  // The arguments that the user enters
   private String[] args;
   private String arg = "";
-  public LS(JFileSystem fileManager, String[] name){
+
+  /**
+   * The constructor, taking ins a JFileSystem and a string array of arguments
+   * 
+   * @param fileManager - The JFilemMnager with all the directories and files
+   * @param name - The argument(s)
+   */
+  public LS(JFileSystem fileManager, String[] name) {
     this.Manager = fileManager;
     this.args = name;
   }
-  public LS(JFileSystem fileManager){
+
+  /**
+   * Secondary constructor if no arguments are given
+   * 
+   * @param fileManager
+   */
+  public LS(JFileSystem fileManager) {
     this.Manager = fileManager;
     this.args = new String[1];
+    // Setting a blank command
     this.args[0] = "";
   }
-  public void execute(){
-    for (int indexarg = 0 ; indexarg < this.args.length; indexarg++){
-      this.arg = args[indexarg];
-    
-      String contents = "";
-      if (arg == ""){
-          Folder currFolder = (Folder) Manager.getObject(Manager.getCurrPath());
-          Vector childNames;
-          if (currFolder == null){
-            childNames = null;
-          }
-          else{
-            childNames = currFolder.getAllChildrenNames();
-          }
-          
-          if (childNames != null){
+
+  /**
+   * A method responsible for displaying the contents of the local directory
+   * Prints each of the contents on a newline (as per A2a hand out)
+   * 
+   * @return contents - Returns a string containing the contents
+   */
+  public String executeNoArg() {
+    String contents = "";
+    // Getting the current folder
+    Folder currFolder = (Folder) Manager.getObject(Manager.getCurrPath());
+    // A vector representing all the names of the children directories to this
+    // one
+    Vector childNames;
+    // If there are no children
+    if (currFolder == null) {
+      childNames = null;
+    } else {
+      // Getting the string vector of all the children stored in the folder
+      childNames = currFolder.getAllChildrenNames();
+    }
+    // Sorts the child names in alphabetical order
+    if (childNames != null) {
+      java.util.Collections.sort(childNames);
+    }
+    int i = 0;
+    if (childNames != null) {
+      // Adding each of the children names to a new string
+      while (i < childNames.size()) {
+        contents = contents + "\n" + childNames.get(i);
+        i++;
+      }
+    }
+    return contents;
+  }
+
+  /**
+   * A method responsible for displaying the contents of a directory or file at
+   * the given path
+   * 
+   * @param arg - The absolute path
+   * @return contents - A string representation of all the children
+   */
+  public String executeFullPath(String arg) {
+    String contents = "";
+    // Checking if the path given is valid
+    if (Manager.checkValidPath(arg)) {
+      // Determining the type of object that has been specified
+      if (Manager.getObject(arg).getClass().equals(Folder.class)) {
+        // Getting the current folder
+        Folder currFolder = (Folder) Manager.getObject(arg);
+        // If the folder does not exist
+        if (currFolder.equals(null)) {
+          Output.printPathError();
+        } else {
+          // All the children in the specified folder
+          Vector childNames = currFolder.getAllChildrenNames();
+          if (childNames != null) {
+            // Sorting them alphabetically
             java.util.Collections.sort(childNames);
           }
           int i = 0;
-          if (childNames != null){
-             while (i < childNames.size()){
-              contents = contents + "\n" + childNames.get(i);
-              i++;
-             }
+          // Adding the child names to a string and formating it
+          while (i < currFolder.getAllChildren().size()) {
+            contents = contents + "     " + childNames.get(i);
+            i++;
           }
-        
+          // Finishing up the formating
+          contents = arg + ": " + contents + "\n";
+        }
+      } else {
+        // If the path specified is to a file
+        contents = arg + "\n";
       }
-      else if (arg.startsWith("/")){
-        if (Manager.checkValidPath(arg)){
-  
-            if (Manager.getObject(arg).getClass().equals(Folder.class)){
-              
-              Folder currFolder = (Folder) Manager.getObject(arg);
-              Vector childNames = currFolder.getAllChildrenNames();
-              if (childNames != null){
-                java.util.Collections.sort(childNames);
-              }
-              int i = 0;
-              while (i < currFolder.getAllChildren().size()){
-                contents = contents + "     " + childNames.get(i);
-                i++;
-              }
-              contents = arg + ": " + contents + "\n";
-            }
-            else {
-              contents = arg + "\n";
-            }
-         
-        }
-        else{
-          Output.printPathError();
-        }
+    } else {
+      // If an incorrect path is given
+      Output.printPathError();
+    }
+    return contents;
+  }
+
+  /**
+   * Accepts a local path is ".." inside as part of it and returns the
+   * corresponding full path
+   * 
+   * @param name - the argument from the user
+   * @return The absolute path of the folder that the user specified
+   */
+  public String removeDots(String name) {
+    // Counting starts from the beginning of the string and moves to the end
+    int headIndex = 0;
+    // The number of occurrences of the specified string
+    int numOfOccurences = 0;
+    // Loop goes until the end of the string
+    while (headIndex != -1) {
+      // The first index of ".." from start to endIndex
+      headIndex = name.indexOf("..", headIndex);
+      // If the specified string ("..") is still found within the above
+      // substring
+      if (headIndex != -1) {
+        // Increment the number of occurrences
+        numOfOccurences++;
+        // Increasing the search by 2, (since ".." has a length of 2)
+        headIndex += 2;
       }
-      else if (arg.startsWith("..")){
-        if (arg.equals("..") || arg.equals("../")){
-          String path = "";
-          if (Manager.getCurrPath().split("/").length > 2){
-            for (int i = 1; i < Manager.getCurrPath().split("/").length - 1; i++){
-              path =  path + "/" +  Manager.getCurrPath().split("/")[i];
-            }
-            if (Manager.checkValidPath(arg)){
-                if (Manager.getObject(arg).getClass().equals(Folder.class)){
-                  Folder currFolder = (Folder) Manager.getObject(arg);
-                  Vector childNames = currFolder.getAllChildrenNames();
-                  if (childNames != null){
-                    java.util.Collections.sort(childNames);
-                  }
-                  int i = 0;
-                  while (i < currFolder.getAllChildren().size()){
-                    contents = contents + "     " + childNames.get(i);
-                    i++;
-                  }
-                  contents = arg + ": " + contents + "\n";
-                }
-                else {
-                  contents = arg + "\n";
-                }
-             
-            }
-            else{
-              Output.printPathError();
-            }
-          }
-          else if (Manager.getCurrPath().equals("/")){
-            Output.printError();
-          }
-        }
-        else{
-          int lastIndex = 0;
-          int count = 0;
-    
-          while(lastIndex != -1){
-    
-              lastIndex = arg.indexOf("..",lastIndex);
-    
-              if(lastIndex != -1){
-                  count ++;
-                  lastIndex += 2;
-              }
-          }
-          String path = "";
-          for (int i = 1; i < Manager.getCurrPath().split("/").length - count; i++){
-            path =  path + "/" +  Manager.getCurrPath().split("/")[i];
-          }
-          arg = path + arg;
-          
-          if (Manager.checkValidPath(arg)){
-              if (Manager.getObject(arg).getClass().equals(Folder.class)){
-                Folder currFolder = (Folder) Manager.getObject(arg);
-                Vector childNames = currFolder.getAllChildrenNames();
-                if (childNames != null){
-                  java.util.Collections.sort(childNames);
-                }
-                int i = 0;
-                while (i < currFolder.getAllChildren().size()){
-                  contents = contents + "     " + childNames.get(i);
-                  i++;
-                }
-                contents = arg + ": " + contents + "\n";
-              }
-              else {
-                contents = arg + "\n";
-              }
-        
-          }
-          else{
-            Output.printPathError();
-          }
+    }
+    // Getting the current working (local) path
+    String path = Manager.getCurrPath();
+    int indexDots = name.indexOf("/");
+    // Runs for the number of times ".." is in the argument
+    for (int i = 0; i < numOfOccurences; i++) {
+      // If name still contains ".." and but does not start with a ".." then
+      // name is in the form ../DIR_NAME/../ , so the DIR_NAME must be added
+      // to the absolute path and "i" must be decremented
+      if (!name.startsWith("..") && name.contains("..")) {
+        path = path + "/" + name.substring(0, indexDots);
+        i--;
+      } else {
+        try { // If the number of ".." reaches past the root and error is thrown
+          // Cutting a section of the current path off
+          path = path.substring(0, path.lastIndexOf("/"));
+        } catch (Exception e) {
+          // Returning an invalid path
+          path = "//////////";
+          break;
         }
       }
-      else if (arg.contains("/")){
-        arg = Manager.getCurrPath() + "/" + arg;
-        if (Manager.checkValidPath(arg)){
-            if (Manager.getObject(arg).getClass().equals(Folder.class)){
-              Folder currFolder = (Folder) Manager.getObject(arg);
-              Vector childNames = currFolder.getAllChildrenNames();
-              if (childNames != null){
-                java.util.Collections.sort(childNames);
-              }
-              int i = 0;
-              while (i < currFolder.getAllChildren().size()){
-                contents = contents + "     " + childNames.get(i);
-                i++;
-              }
-              contents = arg + ": " + contents + "\n";
-            }
-            else{
-              contents = arg + "\n";
-            }
-            
+      // Removing a piece of the argument off
+      name = name.substring(indexDots + 1, name.length());
+      indexDots = name.indexOf("/");
+
+    }
+    // Returning the absolute path
+    return path + "/" + name;
+  }
+
+  /**
+   * The method that will be called by ProQuery. Determines what kind of
+   * argument the user has entered; Runs all the arguments that the user has
+   * entered
+   */
+  public void execute() {
+    // Runs all elements in the string array
+    for (int indexarg = 0; indexarg < this.args.length; indexarg++) {
+      this.arg = args[indexarg];
+      // String representation of the children
+      String contents = "";
+      // If no argument was given
+      if (arg == "") {
+        contents = executeNoArg();
+      } // If an absolute path was given
+      else if (arg.startsWith("/")) {
+        contents = executeFullPath(arg);
+      } // If a path containing ".." was given
+      else if (arg.contains("..")) {
+        // Turning arg into an absolute path
+        arg = this.removeDots(arg);
+        contents = this.executeFullPath(arg);
+      } // If a local path is given
+      else if (arg.contains("/")) {
+        // Creating an absolute path
+        if (!Manager.getCurrPath().equals("/")) {
+          arg = Manager.getCurrPath() + "/" + arg;
+        } else {
+          arg = "/" + arg;
         }
-        else{
-          Output.printPathError();
-        }
-      }
+        contents = this.executeFullPath(arg);
+      } // If a directory name is given
       else {
-       if (Manager.getCurrPath().equals("/")){
-         arg = Manager.getCurrPath() + arg;
-       }
-       else {
-         arg = Manager.getCurrPath() + "/" + arg;
-       }
-       
-       if (Manager.checkValidPath(arg)){
-           //String argPath = Manager.getCurrPath() + "/" + arg;
-           if (Manager.getObject(arg).getClass().equals(Folder.class)){
-             
-             Folder currFolder = (Folder) Manager.getObject(arg);
-             Vector childNames = currFolder.getAllChildrenNames();
-             if (childNames != null){
-               java.util.Collections.sort(childNames);
-             }
-             int i = 0;
-             while (i < currFolder.getAllChildren().size()){
-               contents = contents + "     " + childNames.get(i);
-               i++;
-             }
-             contents = arg + ": " + contents + "\n";
-           }
-           else {
-             contents = arg + "\n";
-           }
-        
-        
-       }
-       else{
-         Output.printPathError();
-       }
+        // Building the absolute path
+        if (Manager.getCurrPath().equals("/")) {
+          arg = Manager.getCurrPath() + arg;
+        } else {
+          arg = Manager.getCurrPath() + "/" + arg;
+        }
+        contents = this.executeFullPath(arg);
       }
+      // Using the output class to print the string
       Output.printString(contents);
     }
   }
-  
+
 }
