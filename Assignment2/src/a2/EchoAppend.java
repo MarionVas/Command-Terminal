d0b64dadc7ec986e1130cee4bdb200f7e2efd858
gaoxu1@UTORONTO.CA <gaoxu1@UTORONTO.CA@fb2d0955-5374-4267-af28-b32595e4e7c6>
2016-06-24 01:52:30 +0000

    "Corrected error that didn't allow inheritance as wanted"

--- Assignment2/src/a2/EchoAppend.java
+++ Assignment2/src/a2/EchoAppend.java
@@ -4,36 +4,46 @@ public class EchoAppend extends EchoOverwrite implements CommandInterface {
 
   /**
    * The constructor uses the constructor from EchoOverwrite
-   * @param jFileSystem 
+   * 
+   * @param jFileSystem
    */
+
   public EchoAppend(JFileSystem manager, String[] parameter) {
     super(manager, parameter);
   }
 
-  /**
-   * This function will return the name of the outfile given the parameter given
-   * to the constructor
-   * 
-   * @return fileName - The name of the outfile
-   */
-
-  private String findFileName() {
-    String fileName = getParameter().substring(getIndexOfSymbol() + 2,
-        getParameter().length());
-    return fileName;
+  @Override
+  public void execute() {
+    // check if an outfile is specified
+    if (getParameter().length == 1) {
+      // if an outfile is not specified, display the string
+      Output.printString(getParameter()[0]);
+    } else {
+      File replaceFile = super.findFile();
+      append(replaceFile, getParameter()[0]);
+    }
   }
 
-  /**
-   * This function will add string provided in the parameter to the body of the
-   * outfile
-   */
-
-  private void replace(File file, String body) {
+  private void append(File file, String body) {
     file.addToBody(body);
   }
-  
-  /**
-   * This function return the instructions on how to use the command echo 
+
+  /*
+   * /** This function will return the name of the outfile given the parameter
+   * given to the constructor
+   * 
+   * @return fileName - The name of the outfile
+   * 
+   * private String findFileName() { String fileName =
+   * getParameter().substring(getIndexOfSymbol() + 3, getParameter().length());
+   * return fileName; }
+   * 
+   * /** This function will add string provided in the parameter to the body of
+   * the outfile
+   * 
+   * private void replace(File file, String body) { file.addToBody(body); }
+   * 
+   * /** This function return the instructions on how to use the command echo
    * STRING [>> OUTFILE].
    * 
    * @return a string telling users the how the command works
