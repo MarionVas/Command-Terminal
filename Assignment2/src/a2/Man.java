b59066ab34874d66e0f4ade6b815090b075c3c8d
haqueya1@UTORONTO.CA <haqueya1@UTORONTO.CA@fb2d0955-5374-4267-af28-b32595e4e7c6>
2016-06-25 23:05:23 +0000

    Initialized Man command class.

--- Assignment2/src/a2/Man.java
+++ Assignment2/src/a2/Man.java
@@ -1,5 +1,12 @@
 package a2;
 
 public class Man implements CommandInterface { //requires execute() command
+  
+  private JFileSystem jFileSystem;
+  private String[] commandName;
 
+  public Man(JFileSystem jFileSystem, String[] commandName) {
+    this.jFileSystem = jFileSystem;
+    this.commandName = commandName;
+  }
 }
