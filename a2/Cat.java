0497b36d4ffc847675fb1402a8ca06b2aa1a2b3c
gaoxu1 <gaoxu1@UTORONTO.CA>
2016-06-25 20:57:32 +0000

    "documented Cat class and corrected small bug where the command would end if a file is not found"

--- a2/Cat.java
+++ a2/Cat.java
@@ -1,33 +1,54 @@
 package a2;
 
-public class Cat implements CommandInterface { // require execute() method
+public class Cat implements CommandInterface {
   private FileSystem fileSystem;
   private String[] fileNames;
+
+  /**
+   * The constructor
+   * 
+   * @param jFileSystem - The JFileSystem with all the file and folder
+   * @param parameter - The string array with all the file names the user enters
+   */
 
   public Cat(JFileSystem manager, String[] files) {
     this.fileSystem = manager;
     this.fileNames = files;
   }
 
-
+  /**
+   * This method will take the parameter passed into the constructor and read
+   * the contents of those files in the current directory. If the files do not
+   * exist, an error message will be printed
+   */
 
   public void execute() {
+    // get the current working directory
     Folder currFolder = fileSystem.getCurrFolder();
+    // check if the user wants to read one file
     if (this.fileNames.length == 1) {
+      // get the file the user wants to read
       File file = (File) currFolder.getFile(fileNames[0]);
+      // if the file does not exist print an error message
       if (file == null) {
         Output.printFileNameError();
+        // if the file does exist print the contents of the file
       } else {
         Output.printContents(file.getBody());
       }
     } else {
+      // get each file the user wants to print
       for (String eachFile : this.fileNames) {
+        // get the file the user wants to read
         File file = (File) currFolder.getFile(eachFile);
         if (file == null) {
+          // if the file does not exist print an error message
+          // stop running the command
           Output.printFileNameError();
-          break;
         } else {
+          // if the file does exist print the contents of the file
           Output.printContents(file.getBody());
+          // print three lines to separate each file being read
           Output.printString("");
           Output.printString("");
           Output.printString("");
@@ -35,5 +56,4 @@ public class Cat implements CommandInterface { // require execute() method
       }
     }
   }
-
 }
