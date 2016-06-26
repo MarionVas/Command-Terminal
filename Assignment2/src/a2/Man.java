f5a8c6ad3b695918e4247bcdc848afa24594e674
haqueya1@UTORONTO.CA <haqueya1@UTORONTO.CA@fb2d0955-5374-4267-af28-b32595e4e7c6>
2016-06-26 03:51:17 +0000

    Completed implementation of Manual class.

--- Assignment2/src/a2/Man.java
+++ Assignment2/src/a2/Man.java
@@ -1,12 +1,79 @@
 package a2;
 
-public class Man implements CommandInterface { //requires execute() command
-  
-  private JFileSystem jFileSystem;
-  private String[] commandName;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Hashtable;
 
-  public Man(JFileSystem jFileSystem, String[] commandName) {
+public class Man implements CommandInterface {
+
+  private JFileSystem jFileSystem;
+  private String[] commandKey;
+
+  public Man(JFileSystem jFileSystem, String[] commandKey) {
     this.jFileSystem = jFileSystem;
-    this.commandName = commandName;
+    this.commandKey = commandKey;
+  }
+
+  public void execute() {
+
+    Hashtable<String, String> commandNoPara = new Hashtable<String, String>();
+
+    Hashtable<String, String> commandWithPara = new Hashtable<String, String>();
+
+    commandNoPara.put("ls", "a2.LS");
+    commandNoPara.put("exit", "a2.Exit");
+    commandNoPara.put("pwd", "a2.PWD");
+    commandNoPara.put("popd", "a2.PopD");
+
+    commandWithPara.put("cd", "a2.CD");
+    commandWithPara.put("cat", "a2.Cat");
+    commandWithPara.put("echo", "a2.Echo");
+    commandWithPara.put("man", "a2.Man");
+    commandWithPara.put("mkdir", "a2.Mkdir");
+    commandWithPara.put("pushd", "a2.PushD");
+
+    if (commandKey.length == 1 && commandNoPara.containsKey(commandKey[0])) {
+
+      String commandName = commandNoPara.get(commandKey[0]);
+
+      try {
+        CommandInterface commandInstance =
+            (CommandInterface) Class.forName(commandName)
+                .getConstructor(JFileSystem.class).newInstance(jFileSystem);
+        Output.printString(commandInstance.manual());
+
+      } catch (InstantiationException | IllegalAccessException
+          | IllegalArgumentException | InvocationTargetException
+          | NoSuchMethodException | SecurityException
+          | ClassNotFoundException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
+    } else if (commandKey.length == 1
+        && commandWithPara.containsKey(commandKey[0])) {
+      
+      String commandName = commandWithPara.get(commandKey[0]);
+      
+      try {
+        CommandInterface commandInstance =
+            (CommandInterface) Class.forName(commandName)
+                .getConstructor(JFileSystem.class, String[].class)
+                .newInstance(jFileSystem, commandKey);
+        Output.printString(commandInstance.manual());
+        
+      } catch (InstantiationException | IllegalAccessException
+          | IllegalArgumentException | InvocationTargetException
+          | NoSuchMethodException | SecurityException
+          | ClassNotFoundException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
+      
+    } else {
+      Output.printError();
+    }
+  }
+
+  public String manual() {
+    return "man CMD - Displays documentation regarding the specified command\n";
   }
 }
