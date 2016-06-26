786e107b159da28d28af4057d8dbe198d6f2f035
shahid41@UTORONTO.CA <shahid41@UTORONTO.CA@fb2d0955-5374-4267-af28-b32595e4e7c6>
2016-06-26 03:29:03 +0000

    Added two new test cases

--- Assignment2/src/test/CDTest.java
+++ Assignment2/src/test/CDTest.java
@@ -154,5 +154,23 @@ public class CDTest {
     cd1.execute();
     assertEquals("/a/a1", jFileSystem.getCurrPath());
   }
+  
+  @Test
+  public void testExecute9() 
+  {
+    location[0] = "/a/a1/a2";
+    cd1 = new CD(jFileSystem, location);
+    cd1.execute();
+    assertEquals("/a/a1/a2", jFileSystem.getCurrPath());
+  }
+  
+  @Test
+  public void testExecute10() 
+  {
+    location[0] = "/a/a1/../a1/./a2/";
+    cd1 = new CD(jFileSystem, location);
+    cd1.execute();
+    assertEquals("/a/a1/a2", jFileSystem.getCurrPath());
+  }
 
 }
