750f1eb87a5d80c984123beac55b6b403eb98608
vasant42 <MarionVas@users.noreply.github.com>
2016-06-26 07:16:53 +0000

    Removed addFilePath and relativePath checker; Properly formatted as well

--- a2/FileSystem.java
+++ a2/FileSystem.java
@@ -3,21 +3,32 @@ package a2;
 import java.util.*;
 
 public interface FileSystem {
-  public String getCurrPath(); 
+  public String getCurrPath();
+
   public void setFullPath(String newDir);
+
   public Object getObject(String name);
+
   public Object getObjRecurs(String name, String currName, Folder dirrOrFile);
+
   public void add(Folder newFolder);
-  public void addFilePath(String path);
+
   public Vector<String> getFullPaths();
+
   public boolean checkValidPath(String path);
+
   public void setCurrFolder(Folder folder);
+
   public Folder getCurrFolder();
+
   public void addFullPath(String path);
-  public boolean relativePathChecker(String path);
+
   public void setRoot(Folder root);
+
   public Folder getRootFolder();
+
   public DirStack getDirStack();
+
   public void setDirStack(DirStack stack);
 }
 
