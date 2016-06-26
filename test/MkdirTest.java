4aafa9d826643b36526b16f663c199d0b356b58f
shahid41 <shahid41@UTORONTO.CA>
2016-06-26 04:05:07 +0000

    Added one test case to mkdir

--- test/MkdirTest.java
+++ test/MkdirTest.java
@@ -5,14 +5,32 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-public class MkdirTest {
+import a2.*;
+
+public class MkdirTest 
+{
+  private JFileSystem jFileSystem;
+  private String[] fileName;
+  private String[] fileNames;
+  private Mkdir mkdir1;
+  private Mkdir mkdir2;
+  private Mkdir mkdir3;
 
   @Before
-  public void setUp() throws Exception {}
+  public void setUp() throws Exception 
+  {
+    jFileSystem = new JFileSystem();
+    fileName = new String[1];
+    fileNames = new String[3];
+  }
 
   @Test
-  public void test() {
-    fail("Not yet implemented");
+  public void testExecute1() 
+  {
+    fileName[0] = "a";
+    mkdir1 = new Mkdir(jFileSystem, fileName);
+    mkdir1.execute();
+    assertTrue(jFileSystem.checkValidPath("/a"));
   }
 
 }
