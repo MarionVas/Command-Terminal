86adb4add357acae13af2e58cb158979e9c74009
haqueya1@UTORONTO.CA <haqueya1@UTORONTO.CA@fb2d0955-5374-4267-af28-b32595e4e7c6>
2016-06-26 04:39:13 +0000

    Changed implementation with how PWD interacts with Output class.

--- Assignment2/src/a2/PWD.java
+++ Assignment2/src/a2/PWD.java
@@ -3,14 +3,21 @@ package a2;
 public class PWD implements CommandInterface{
   private FileSystem fileSystem;
   private String path;
+  private String stringToOutput;
+  
   public PWD(JFileSystem manager){
     this.fileSystem = manager;
   }
   public void execute() {
-    Output.printString(fileSystem.getCurrPath());
+    stringToOutput = fileSystem.getCurrPath();
+    Output.printString(stringToOutput);
   }
   
   public String manual() {
     return "pwd - Prints the current working directory with its whole path.\n";
+  }
+  
+  public String getStringToOutput() {
+    return stringToOutput;
   }
 }
