f6b337b36a3cfd5a4b4d73429d9ca061713e2da0
haqueya1 <haqueya1@UTORONTO.CA>
2016-06-26 07:19:41 +0000

    Fixed parameter splicing for Echo class.

--- a2/Echo.java
+++ a2/Echo.java
@@ -1,5 +1,7 @@
 package a2;
 
+import java.util.Arrays;
+
 public class Echo implements CommandInterface {
   private JFileSystem jFileSystem;
   private String[] echoParameters;
@@ -7,7 +9,15 @@ public class Echo implements CommandInterface {
 
   public Echo(JFileSystem jFileSystem, String[] echoParameters) {
     this.jFileSystem = jFileSystem;
-    this.echoParameters = echoParameters;
+    
+    if (java.util.Arrays.asList(echoParameters).contains(">")) {
+      this.echoParameters = Echo.fixEchoParameters(echoParameters, ">");
+    } else if (java.util.Arrays.asList(echoParameters).contains(">>")) {
+      this.echoParameters = Echo.fixEchoParameters(echoParameters, ">>");
+    } else {
+      this.echoParameters = echoParameters;
+    }
+    System.out.println(Arrays.toString(this.echoParameters));
   }
 
   public void execute() {
@@ -15,15 +25,43 @@ public class Echo implements CommandInterface {
       EchoOverwrite overwriteFile =
           new EchoOverwrite(jFileSystem, echoParameters);
       overwriteFile.execute();
+      
     } else if (echoParameters[1].equals(">>")) {
       EchoAppend appendFile = new EchoAppend(jFileSystem, echoParameters);
       appendFile.execute();
+      
     } else if (echoParameters.length == 1) {
       stringToOutput = echoParameters[0];
       Output.printString(stringToOutput);
+      
     } else {
       Output.printError();
     }
+  }
+  
+  public static String[] fixEchoParameters(String[] echoParameters, String echoSign) {
+    String[] fixedEchoParameters = new String[3];
+    int echoSignIndex = java.util.Arrays.asList(echoParameters).indexOf(echoSign);
+    
+    String[] preSign = Arrays.copyOfRange(echoParameters, 0, echoSignIndex);
+    String[] postSign = Arrays.copyOfRange(echoParameters, echoSignIndex + 1, echoParameters.length);
+    
+    fixedEchoParameters[0] = Echo.joinStrWithSpace(preSign);
+    fixedEchoParameters[1] = echoSign;
+    fixedEchoParameters[2] = Echo.joinStrWithSpace(postSign);
+    
+    return fixedEchoParameters;
+  }
+  
+  public static String joinStrWithSpace(String[] strPara) {
+    StringBuilder newString = new StringBuilder();
+    for (int i=0; i<strPara.length; i++) {
+      if (i > 0) {
+        newString.append(" ");
+      }
+      newString.append(strPara[i]);
+    }
+    return newString.toString();
   }
   
   public String manual() {
