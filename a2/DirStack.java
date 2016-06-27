7980d95a6bfc7ba03d830bd587c7f600e23abb7d
shahid41 <shahid41@UTORONTO.CA>
2016-06-27 13:54:40 +0000

    Formatted properly

--- a2/DirStack.java
+++ a2/DirStack.java
@@ -2,29 +2,24 @@ package a2;
 
 import java.util.Stack;
 
-public class DirStack 
-{
+public class DirStack {
   public Stack<String> directoryStack;
 
   // Constructor for the DirStack
-  public DirStack()
-  {
+  public DirStack() {
     // make the directory stack
     directoryStack = new Stack<String>();
   }
-  
-  public void pushD(String dir)
-  {
+
+  public void pushD(String dir) {
     directoryStack.push(dir);
   }
-  
-  public Stack<String> getStack()
-  {
+
+  public Stack<String> getStack() {
     return directoryStack;
   }
-  
-  public String popD()
-  {
+
+  public String popD() {
     return directoryStack.pop();
   }
 }
