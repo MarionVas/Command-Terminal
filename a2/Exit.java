1231f6bcb0020d943d4f04ef1d84c3656b735044
haqueya1 <haqueya1@UTORONTO.CA>
2016-06-27 18:29:19 +0000

    Formatted exit class.

--- a2/Exit.java
+++ a2/Exit.java
@@ -1,24 +1,26 @@
 package a2;
 
 public class Exit implements CommandInterface {
-    private FileSystem manager;
-    public Exit(JFileSystem man){
-      this.manager = man;
-      
-    }
-	public void execute() {
-		// method to exit consoles
-		System.exit(0);
-	}
-	
-	/**
-	   * This function return the instructions on how to use the command exit.
-	   * 
-	   * @return a string telling users the how the command works
-	   */
-	
-	public String manual(){
-	  return "exit - Terminates the session/program.\n";
-	}
+  private FileSystem manager;
+
+  public Exit(JFileSystem man) {
+    this.manager = man;
+
+  }
+
+  public void execute() {
+    // method to exit consoles
+    System.exit(0);
+  }
+
+  /**
+   * This function return the instructions on how to use the command exit.
+   * 
+   * @return a string telling users the how the command works
+   */
+
+  public String manual() {
+    return "exit - Terminates the session/program.\n";
+  }
 
 }
