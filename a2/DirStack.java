7cb91c9f21681243d4864e0c71c45e74f3226ed5
shahid41 <shahid41@UTORONTO.CA>
2016-06-27 13:58:50 +0000

    Added some documentation

--- a2/DirStack.java
+++ a2/DirStack.java
@@ -11,14 +11,31 @@ public class DirStack {
     directoryStack = new Stack<String>();
   }
 
+  /**
+   * The purpose of this method is to add an item to the directory stack
+   * 
+   * @param dir: the string value of the item the user wishes to add
+   */
   public void pushD(String dir) {
     directoryStack.push(dir);
   }
 
+  /**
+   * The purpose of this method is to return the directoryStack, used primarily
+   * for testing purposes
+   * 
+   * @return Stack<String> directoryStack the returned stack
+   */
   public Stack<String> getStack() {
     return directoryStack;
   }
 
+  /**
+   * The purpose of this method is to return the last value of the
+   * directoryStack as per LIFO
+   * 
+   * @return A string value which was the last item of the stack
+   */
   public String popD() {
     return directoryStack.pop();
   }
