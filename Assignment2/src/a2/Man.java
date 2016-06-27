78035091755474c8687bb391cec78b9011316b12
haqueya1@UTORONTO.CA <haqueya1@UTORONTO.CA@fb2d0955-5374-4267-af28-b32595e4e7c6>
2016-06-27 02:47:06 +0000

    Updated comments/documentation.

--- Assignment2/src/a2/Man.java
+++ Assignment2/src/a2/Man.java
@@ -4,26 +4,42 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.Hashtable;
 
 public class Man implements CommandInterface {
-
+  // JFileSystemw which the ProQuery will pass in
   private JFileSystem jFileSystem;
+  // The command whose documentation will be printed onto the shell
   private String[] commandKey;
+  // The string that will be sent to the output class to be printed to shell
+  private String stringToOutput;
 
+  /**
+   * Constructs a Man object whose purpose is to provide documentation for a
+   * specified command class.
+   * 
+   * @param jFileSystem FileSystem that the commands may act upon.
+   * @param commandKey Specified command whose documentation will be printed.
+   */
   public Man(JFileSystem jFileSystem, String[] commandKey) {
     this.jFileSystem = jFileSystem;
     this.commandKey = commandKey;
   }
 
+  /**
+   * The purpose of this method is to allow user to print the documentation of a
+   * command class that was specified during the construction of the Man object.
+   */
   public void execute() {
-
+    // Initialize a hashtable for commands without parameters
     Hashtable<String, String> commandNoPara = new Hashtable<String, String>();
-
+    // Initialize a hashtable for commands with parameters
     Hashtable<String, String> commandWithPara = new Hashtable<String, String>();
 
+    // Populate the hashtable for commands without parameters
     commandNoPara.put("ls", "a2.LS");
     commandNoPara.put("exit", "a2.Exit");
     commandNoPara.put("pwd", "a2.PWD");
     commandNoPara.put("popd", "a2.PopD");
 
+    // Populate the hastable for commands with parameters
     commandWithPara.put("cd", "a2.CD");
     commandWithPara.put("cat", "a2.Cat");
     commandWithPara.put("echo", "a2.Echo");
@@ -31,48 +47,70 @@ public class Man implements CommandInterface {
     commandWithPara.put("mkdir", "a2.Mkdir");
     commandWithPara.put("pushd", "a2.PushD");
 
+    // If one command is given and it is a command which requires no parameters
     if (commandKey.length == 1 && commandNoPara.containsKey(commandKey[0])) {
-
+      // Acquire the commands class name in string form
       String commandName = commandNoPara.get(commandKey[0]);
 
       try {
+        // Create an instance of the specified command whose constructor
+        // requires only a JFileSystem
         CommandInterface commandInstance =
             (CommandInterface) Class.forName(commandName)
                 .getConstructor(JFileSystem.class).newInstance(jFileSystem);
-        Output.printString(commandInstance.manual());
+        // Call the manual method of the instance and send the acquired string
+        // to the output to be printed
+        stringToOutput = commandInstance.manual();
+        Output.printString(stringToOutput);
 
+        // Catch any unwanted cases/exceptions
       } catch (InstantiationException | IllegalAccessException
           | IllegalArgumentException | InvocationTargetException
           | NoSuchMethodException | SecurityException
           | ClassNotFoundException e) {
-        // TODO Auto-generated catch block
         e.printStackTrace();
       }
+
+      // If one command is given and it is a command which requires parameters
     } else if (commandKey.length == 1
         && commandWithPara.containsKey(commandKey[0])) {
-      
+
+      // Acquire the commands class name in string form
       String commandName = commandWithPara.get(commandKey[0]);
-      
+
       try {
+        // Create an instance of the specified command whose constructor
+        // requires a JFileSystem and a string array
         CommandInterface commandInstance =
             (CommandInterface) Class.forName(commandName)
                 .getConstructor(JFileSystem.class, String[].class)
                 .newInstance(jFileSystem, commandKey);
-        Output.printString(commandInstance.manual());
-        
+        // Call the manual method of the instance and send the acquired string
+        // to the output to be printed
+        stringToOutput = commandInstance.manual();
+        Output.printString(stringToOutput);
+
+        // Catch any unwanted cases/exceptions
       } catch (InstantiationException | IllegalAccessException
           | IllegalArgumentException | InvocationTargetException
           | NoSuchMethodException | SecurityException
           | ClassNotFoundException e) {
-        // TODO Auto-generated catch block
         e.printStackTrace();
       }
-      
+
+      // If numerous commands were specified, or an invalid command name
     } else {
+      // Notify the user that they did not provide a valid command
       Output.printError();
     }
   }
 
+  /**
+   * The purpose of this method is to return a string containing the
+   * documentation/information regarding the "man" command.
+   * 
+   * @return String
+   */
   public String manual() {
     return "man CMD - Displays documentation regarding the specified command\n";
   }
