e12c2d9dc45e876bd32472630c664b6c80d9a08f
haqueya1@UTORONTO.CA <haqueya1@UTORONTO.CA@fb2d0955-5374-4267-af28-b32595e4e7c6>
2016-06-27 18:27:08 +0000

    Formatted PWD class.

--- Assignment2/src/a2/PWD.java
+++ Assignment2/src/a2/PWD.java
@@ -1,22 +1,23 @@
 package a2;
 
-public class PWD implements CommandInterface{
+public class PWD implements CommandInterface {
   private FileSystem fileSystem;
   private String path;
   private String stringToOutput;
-  
-  public PWD(JFileSystem manager){
+
+  public PWD(JFileSystem manager) {
     this.fileSystem = manager;
   }
+
   public void execute() {
     stringToOutput = fileSystem.getCurrPath();
     Output.printString(stringToOutput);
   }
-  
+
   public String manual() {
     return "pwd - Prints the current working directory with its whole path.\n";
   }
-  
+
   public String getStringToOutput() {
     return stringToOutput;
   }
