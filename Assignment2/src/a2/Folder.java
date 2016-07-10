ac59c10abf329cd00936bcd58a39e0af71694e76
vasant42@UTORONTO.CA <vasant42@UTORONTO.CA@fb2d0955-5374-4267-af28-b32595e4e7c6>
2016-07-10 03:20:48 +0000

    Made Folder inherit from Item; moved some methods from Folder to Item

--- Assignment2/src/a2/Folder.java
+++ Assignment2/src/a2/Folder.java
@@ -2,14 +2,11 @@ package a2;
 
 import java.util.*;
 
-public class Folder extends File {
+public class Folder extends Item {
   // The absolute path of the folder
   private String dirPath;
   // A vector containing all the children Folder/Files
-  private Vector children;
   // A vector containing all the names of the sub folders/files
-  private Vector<String> childrenNames;
-
   /**
    * The constructor
    * 
@@ -18,43 +15,18 @@ public class Folder extends File {
    */
   public Folder(String name, String path) {
     // Calling the parent class
-    super(name);
-    this.dirPath = path;
-    this.children = new Vector();
-    this.childrenNames = new Vector();
+    super(name, path, new Vector(), new Vector<String>());
   }
-
-  /**
-   * A getting used to retrieve the absolute path of the folder
-   * 
-   * @return
-   */
-  public String getPath() {
-    return this.dirPath;
-  }
-
   /**
    * Adds children (A Folder) to this Folder
    * 
    * @param child - A sub-Folder to this one
    */
-  public void addChildren(Folder child) {
+  public void addChildren(Item child) {
     // Adding the object
-    this.children.addElement(child);
+    super.getChildren().addElement(child);
     // Adding the Folder name
-    this.childrenNames.addElement(child.getName());
-  }
-
-  /**
-   * Adds children (A File) to this Folder
-   * 
-   * @param child - A sub-File to this one
-   */
-  public void addChildren(File child) {
-    // Adding the object
-    children.addElement(child);
-    // Adding the File name
-    childrenNames.addElement(child.getName());
+    super.getChildrenNames().addElement(child.getName());
   }
 
   /**
@@ -64,11 +36,11 @@ public class Folder extends File {
    * @param index - an index value
    * @return - A Folder or File Object
    */
-  public Object getChildren(int index) {
-    if (children.size() <= index) {
+  public Item getChildren(int index) {
+    if (super.getChildren().size() <= index) {
       return null;
     }
-    return children.get(index);
+    return super.getChildren().get(index);
   }
 
   /**
@@ -78,11 +50,11 @@ public class Folder extends File {
    * @return result - A file
    */
   public File getFile(String name) {
-    Object result = null;
+    File result = null;
     // Checks for all the children in this Folder
-    for (Object fileOrFolder : children) {
-      if (((File) fileOrFolder).getName().equals(name)) {
-        result = fileOrFolder;
+    for (Item fileOrFolder : super.getChildren()) {
+      if (fileOrFolder.getName().equals(name)) {
+        result = (File) fileOrFolder;
       }
     }
     return (File) result;
@@ -95,37 +67,11 @@ public class Folder extends File {
    * @param index - an index value
    * @return - A string representing the name of a object
    */
-  public Object getChildrenName(int index) {
-    if (childrenNames.size() <= index) {
+  public String getChildrenName(int index) {
+    if (super.getChildrenNames().size() <= index) {
       return null;
     }
-    return childrenNames.get(index);
-  }
-
-  /**
-   * A getter used to retrieve the entire vector containing the child objects;
-   * Returns null if no children are present
-   * 
-   * @return children - A vector containing child object
-   */
-  public Vector getAllChildren() {
-    if (children.size() == 0) {
-      return null;
-    }
-    return children;
-  }
-
-  /**
-   * A getter used to retrieve the entire vector containing the child objects
-   * names; Returns null if there are no children present
-   * 
-   * @return children - A vector containing child object names
-   */
-  public Vector getAllChildrenNames() {
-    if (childrenNames.size() == 0) {
-      return null;
-    }
-    return childrenNames;
+    return super.getChildrenNames().get(index);
   }
 
   /**
