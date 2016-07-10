e6cd4705a1831079cac8d9fba938069a1704308a
vasant42@UTORONTO.CA <vasant42@UTORONTO.CA@fb2d0955-5374-4267-af28-b32595e4e7c6>
2016-07-10 03:19:51 +0000

    A new class which both Folder and File will inherit from; Stores the children, name, and path of the Folders/Files

---
+++ Assignment2/src/a2/Item.java
@@ -0,0 +1,37 @@
+package a2;
+
+import java.util.Vector;
+
+public class Item {
+    private Vector<Item> children;
+    private Vector<String> childrenNames;
+    private String name;
+    private String path;
+    
+    public Item(String name, String path,Vector children, Vector<String> childrenName){
+      this.name = name;
+      this.path = path;
+      this.children = children;
+      this.childrenNames = childrenName;
+    }
+
+    public String getPath() {
+      return path;
+    }
+
+    public void setPath(String path) {
+      this.path = path;
+    }
+
+    public Vector<Item> getChildren() {
+      return children;
+    }
+
+    public Vector<String> getChildrenNames() {
+      return childrenNames;
+    }
+
+    public String getName() {
+      return name;
+    }
+}
