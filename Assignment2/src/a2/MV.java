f00be673b68c428fe19ea42f86d1f0f2baea6e87
haqueya1@UTORONTO.CA <haqueya1@UTORONTO.CA@fb2d0955-5374-4267-af28-b32595e4e7c6>
2016-07-14 13:25:39 +0000

    Completed MV implementation

--- Assignment2/src/a2/MV.java
+++ Assignment2/src/a2/MV.java
@@ -3,16 +3,35 @@ package a2;
 public class MV implements CommandInterface {
   private JFileSystem insertedFileSystem;
   private String[] classPaths;
-  
+
 
   public MV(JFileSystem insertedFileSystem, String[] classPaths) {
     this.insertedFileSystem = insertedFileSystem;
     this.classPaths = classPaths;
   }
-  
+
   public void execute() {
-    // TODO Auto-generated method stub
-    
+    try {
+      if (classPaths.length == 2) {
+        String existingItemPath = insertedFileSystem.getFullPath(classPaths[0]);
+        String newItemPath = insertedFileSystem.getFullPath(classPaths[1]);
+        String parentPathExisting = existingItemPath.substring(0, existingItemPath.lastIndexOf("/"));
+        String parentPathNew = newItemPath.substring(0, newItemPath.lastIndexOf("/"));
+        
+        Item existingItem = insertedFileSystem.getObject(existingItemPath);
+        Item parentExisting = insertedFileSystem.getObject(parentPathExisting);
+        Item parentNew = insertedFileSystem.getObject(parentPathNew);
+        
+        ((Folder) parentExisting).removeChildren(existingItem);
+        existingItem.setPath(newItemPath);
+        ((Folder) parentNew).addChildren(existingItem);
+      
+      } else {
+        System.out.print("Invalid command");
+      }
+    } catch (InvalidPath e) {
+      System.out.print("Invalid path");
+    }
   }
 
   public String manual() {
