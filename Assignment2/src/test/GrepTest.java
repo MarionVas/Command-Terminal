0eb6882e2c3557c85dbcae937d567c95bb8003c2
shahid41@UTORONTO.CA <shahid41@UTORONTO.CA@fb2d0955-5374-4267-af28-b32595e4e7c6>
2016-07-15 22:20:55 +0000

    Edited for testcase

--- Assignment2/src/test/GrepTest.java
+++ Assignment2/src/test/GrepTest.java
@@ -42,69 +42,69 @@ public class GrepTest {
     rootFolder.addChildren(file2);
     file3 = new File("file3");
     file3.setPath("/file3");
-    file3.setBody("This is a test in test3,\nAnd it works\nI like This is.");
+    file3.setBody("\na\nb\nc");
     rootFolder.addChildren(file3);
     jFileSystem.addFullPath("/file1");
     jFileSystem.addFullPath("/file2");
     jFileSystem.addFullPath("/file3");
   }
 
-//  @Test
-//  public void test() 
-//  {
-//    //System.out.println(jFileSystem.checkValidPath("/file3"));
-//    String[] command = {"And", "/file3"};
-//    grep = new Grep(jFileSystem, command);
-//    grep.execute();
+  @Test
+  public void test() 
+  {
+    //System.out.println(jFileSystem.checkValidPath("/file3"));
+    String[] command = {"a", "/file3"};
+    grep = new Grep(jFileSystem, command);
+    System.out.println(grep.execute());
 //    //System.out.println(jFileSystem.checkValidPath("/file3"));
 //    String[] command1 = {"This is", "/file3"};
 //    grep = new Grep(jFileSystem, command1);
-//    grep.execute();
-//    assertEquals(true, true);
-//  }
-  
-  // MAKE IT HERE
-  @Test
-  public void test1() 
-  {
-    //System.out.println(jFileSystem.checkValidPath("/file3"));
-
-    
-    
-    String[] folder = {"folder1"};
-    Mkdir dir = new Mkdir(jFileSystem, folder);
-    dir.execute();
-    
-    // ADD THIS FILE INTO THE FOLDER CREATED UP THERE
-    file4 = new File("file4");
-    file4.setPath("/folder1/file4");
-    file4.setBody("This is a test in test4,\nAnd it works\nI like That");
-    file5 = new File("file5");
-    file5.setPath("/folder1/file5");
-    file5.setBody("This is a test in test5,\nAnd it works\nI like This is");
-    
-    Folder directory = (Folder)jFileSystem.getObject("/folder1");
-    directory.addChildren(file4);
-    
-    jFileSystem.addFullPath("/folder1/file4");
-    
-    directory.addChildren(file5);
-    
-    jFileSystem.addFullPath("/folder1/file5");
-    
-    System.out.println(jFileSystem.checkValidPath("/folder1/file4"));
-    
-    
-    
-    // PLS
-    
-    
-
-    String[] command = {"-R", "This is", "/folder1"};
-    grep = new Grep(jFileSystem, command);
-    grep.execute();
-
+//    System.out.println("START" + grep.execute());
     assertEquals(true, true);
   }
+  
+//  // MAKE IT HERE
+//  @Test
+//  public void test1() 
+//  {
+//    //System.out.println(jFileSystem.checkValidPath("/file3"));
+//
+//    
+//    
+//    String[] folder = {"folder1"};
+//    Mkdir dir = new Mkdir(jFileSystem, folder);
+//    dir.execute();
+//    
+//    // ADD THIS FILE INTO THE FOLDER CREATED UP THERE
+//    file4 = new File("file4");
+//    file4.setPath("/folder1/file4");
+//    file4.setBody("This is a test in test4,\nAnd it works\nI like That");
+//    file5 = new File("file5");
+//    file5.setPath("/folder1/file5");
+//    file5.setBody("This is a test in test5,\nAnd it works\nI like This is");
+//    
+//    Folder directory = (Folder)jFileSystem.getObject("/folder1");
+//    directory.addChildren(file4);
+//    
+//    jFileSystem.addFullPath("/folder1/file4");
+//    
+//    directory.addChildren(file5);
+//    
+//    jFileSystem.addFullPath("/folder1/file5");
+//    
+//    System.out.println(jFileSystem.checkValidPath("/folder1/file4"));
+//    
+//    
+//    
+//    // PLS
+//    
+//    
+//
+//    String[] command = {"-R", "This is", "/folder1"};
+//    grep = new Grep(jFileSystem, command);
+//    grep.execute();
+//
+//    assertEquals(true, true);
+//  }
 
 }
