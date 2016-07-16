88052a40d1c1368e243cc5a263e512f7550a2028
gaoxu1 <gaoxu1@UTORONTO.CA>
2016-07-16 02:17:50 +0000

    "completed echo to match new requirements"

--- a2/Echo.java
+++ a2/Echo.java
@@ -6,75 +6,34 @@ public class Echo implements CommandInterface {
   private JFileSystem jFileSystem;
   private String[] echoParameters;
   private String stringToOutput = "";
-  private String echoType = null;
-  private String string;
 
   public Echo(JFileSystem jFileSystem, String[] echoParameters) {
     this.jFileSystem = jFileSystem;
-    string = echoParameters[0].substring(1, echoParameters[0].length() - 1);
-    if (java.util.Arrays.asList(echoParameters).contains(">")) {
-      this.echoParameters = fixEchoParameters(echoParameters, ">");
-    } else if (java.util.Arrays.asList(echoParameters).contains(">>")) {
-      this.echoParameters = fixEchoParameters(echoParameters, ">>");
-    } else {
-      this.echoParameters = echoParameters;
-    }
+    this.echoParameters = echoParameters;
   }
 
   public String execute() {
     if (echoParameters.length == 1) {
-      stringToOutput = string;
-      Output.printString(stringToOutput);
-    } else if (echoParameters[1].equals(">")) {
-      setEchoType("overwrite");
-      EchoOverwrite overwriteFile =
-          new EchoOverwrite(jFileSystem, echoParameters);
-      overwriteFile.execute();
-
-    } else if (echoParameters[1].equals(">>")) {
-      setEchoType("append");
-      EchoAppend appendFile = new EchoAppend(jFileSystem, echoParameters);
-      appendFile.execute();
-    } else {
-      Output.printError();
-    }
-    return "";
-  }
-
-  private String[] fixEchoParameters(String[] echoParameters, String echoSign) {
-    String[] fixedEchoParameters = new String[3];
-    int echoSignIndex =
-        java.util.Arrays.asList(echoParameters).indexOf(echoSign);
-
-    String[] preSign = Arrays.copyOfRange(echoParameters, 0, echoSignIndex);
-    String[] postSign = Arrays.copyOfRange(echoParameters, echoSignIndex + 1,
-        echoParameters.length);
-
-    fixedEchoParameters[0] = joinStrWithSpace(preSign);
-    fixedEchoParameters[1] = echoSign;
-    fixedEchoParameters[2] = joinStrWithSpace(postSign);
-
-    return fixedEchoParameters;
-  }
-
-  private String joinStrWithSpace(String[] strPara) {
-    StringBuilder newString = new StringBuilder();
-    for (int i = 0; i < strPara.length; i++) {
-      if (i > 0) {
-        newString.append(" ");
+      if (echoParameters[0].startsWith("\"")
+          && echoParameters[0].endsWith("\"")) {
+        stringToOutput =
+            echoParameters[0].substring(1, echoParameters[0].length() - 1);
+      } else {
+        System.out.println("That was not a valid String");
       }
-      newString.append(strPara[i]);
+    } else {
+      if (echoParameters[0].startsWith("\"")
+          && echoParameters[echoParameters.length - 1].endsWith("\"")) {
+        for (String eachString : echoParameters) {
+          stringToOutput += eachString + " ";
+        }
+      } else {
+        System.out.println("That was not a valid String");
+      }
     }
-    return newString.toString();
+    return stringToOutput;
   }
 
-  private void setEchoType(String echoType) {
-    this.echoType = echoType;
-  }
-
-  public String getEchoType() {
-    return this.echoType;
-  }
 
   public String manual() {
     EchoOverwrite overwriteFile =
@@ -84,13 +43,4 @@ public class Echo implements CommandInterface {
     return overwriteFile.manual() + "\n" + appendFile.manual();
   }
 
-  /**
-   * This method return the body of the file as a String to be printed on the
-   * console
-   * 
-   * @return stringToOutput - the body of the file that needs to be printed
-   */
-  public String getStringToOutput() {
-    return stringToOutput;
-  }
 }
