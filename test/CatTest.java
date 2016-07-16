964b2283702b275387a9e0a330b27b7a655f6412
gaoxu1 <gaoxu1@UTORONTO.CA>
2016-07-16 01:07:48 +0000

    "added new redirection of output for cat command"

--- test/CatTest.java
+++ test/CatTest.java
@@ -15,6 +15,10 @@ public class CatTest {
   private File file2;
   private File file3;
   private String[] fileNames;
+  private String stringToOutput;
+  private ProQuery runCommand;
+  private Cat concatenate;
+  private String[] files = {"fileContent.txt"};
 
   @Before
   public void setUp() throws Exception {
@@ -22,6 +26,9 @@ public class CatTest {
     Folder rootFolder = new Folder("/", "/");
     jFileSystem.setRoot(rootFolder);
     jFileSystem.setCurrFolder(rootFolder);
+
+    this.runCommand = new ProQuery(this.jFileSystem);
+    this.concatenate = new Cat(this.jFileSystem, files);
 
     file1 = new File("test1");
     rootFolder.addChildren(file1);
@@ -34,7 +41,7 @@ public class CatTest {
   }
 
   @Test
-  public void testExecute1() {
+  public void testCatSingleExistingEmptyFile() {
     /*
      * test to use cat to read an existing new file's contents
      * 
@@ -45,11 +52,11 @@ public class CatTest {
     fileNames[0] = "test1";
     cat = new Cat(jFileSystem, fileNames);
     cat.execute();
-    assertEquals(cat.getStringToOutput(), "");
+    assertEquals(cat.execute(), "");
   }
 
   @Test
-  public void testExecute2() {
+  public void testCatSingleExistingFile() {
     /*
      * test to use cat to read an existing file's contents that holds a String
      * 
@@ -61,11 +68,11 @@ public class CatTest {
     fileNames[0] = "test2";
     cat = new Cat(jFileSystem, fileNames);
     cat.execute();
-    assertEquals(cat.getStringToOutput(), "This is a test in test2.");
+    assertEquals(cat.execute(), "This is a test in test2.");
   }
 
   @Test
-  public void testExecute3() {
+  public void testCatSingleExistingMultipleLinesFile() {
     /*
      * test to use cat to read an existing file's contents that hold multiple
      * lines
@@ -78,12 +85,11 @@ public class CatTest {
     fileNames[0] = "test3";
     cat = new Cat(jFileSystem, fileNames);
     cat.execute();
-    assertEquals(cat.getStringToOutput(),
-        "This is a test in test3,\nAnd it works.");
+    assertEquals(cat.execute(), "This is a test in test3,\nAnd it works.");
   }
 
   @Test
-  public void testExecute4() {
+  public void testCatSingleNonexistingFile() {
     /*
      * test to use cat to read an non-existing file's contents
      * 
@@ -92,13 +98,13 @@ public class CatTest {
     fileNames = new String[1];
     fileNames[0] = "test4";
     cat = new Cat(jFileSystem, fileNames);
-    cat.execute();
-    assertEquals(cat.getStringToOutput(), "");
+    stringToOutput = cat.execute();
+    assertEquals(stringToOutput, "");
   }
 
   // read two existing files
   @Test
-  public void testExecute5() {
+  public void testCatMultipleExistingFiles() {
     /*
      * test to use cat to read multiple existing file's contents
      * 
@@ -110,14 +116,14 @@ public class CatTest {
     fileNames[1] = "test3";
     cat = new Cat(jFileSystem, fileNames);
     cat.execute();
-    assertEquals(cat.getStringToOutput(),
+    assertEquals(cat.execute(),
         "This is a test in test2.\n\n\n\nThis is a test in test3,"
             + "\nAnd it works.\n\n\n\n");
   }
 
   // read existing and non-existing files
   @Test
-  public void testExecute6() {
+  public void testCatMultipleExistingNonexistingFiles() {
     /*
      * test to use cat to read multiple existing and non-existing file's
      * contents
@@ -130,14 +136,14 @@ public class CatTest {
     fileNames[1] = "test4";
     fileNames[2] = "test3";
     cat = new Cat(jFileSystem, fileNames);
-    cat.execute();
-    assertEquals(cat.getStringToOutput(),
+    stringToOutput = cat.execute();
+    assertEquals(stringToOutput,
         "This is a test in test2.\n\n\n\nThis is a test in test3,"
             + "\nAnd it works.\n\n\n\n");
   }
 
   @Test
-  public void testExecute7() {
+  public void testCatMultipleExistingNonexistingEmptyFiles() {
     /*
      * test to use cat to read multiple existing and non-existing file's
      * contents
@@ -151,9 +157,41 @@ public class CatTest {
     fileNames[2] = "test4";
     fileNames[3] = "test3";
     cat = new Cat(jFileSystem, fileNames);
-    cat.execute();
-    assertEquals(cat.getStringToOutput(),
+    stringToOutput = cat.execute();
+    assertEquals(stringToOutput,
         "\n\n\n\nThis is a test in test2.\n\n\n\nThis is a test in test3,"
             + "\nAnd it works.\n\n\n\n");
+  }
+
+  @Test
+  public void testCatRedirectExistingFile() {
+    /*
+     * Test to determine if redirecting output of cat command of an existing
+     * file works. Creates a new file called fileContent.txt, adds the output to
+     * the body of the new file.
+     * 
+     * Expected content of fileContent.txt should be a string containing the
+     * contents of the file test2
+     */
+    runCommand.sortQuery("cat test2 > fileContent.txt");
+    concatenate = new Cat(runCommand.getFileSystem(), files);
+    assertEquals("This is a test in test2.", concatenate.execute());
+  }
+
+  @Test
+  public void testCatRedirectAppendExistingFile() {
+    /*
+     * Test to determine if appending the output of cat command of existing file
+     * to an existing path.txt file works. Creates a new file called
+     * fileContent.txt, adds both outputs to the body of the new file.
+     * 
+     * Expected content of fileContent.txt should be a string containing the
+     * contents of the two files, test2 and test3
+     */
+    runCommand.sortQuery("cat test2 > fileContent.txt");
+    runCommand.sortQuery("cat test3 >> fileContent.txt");
+    concatenate = new Cat(runCommand.getFileSystem(), files);
+    assertEquals("This is a test in test2.\nThis is a test in test3,"
+        + "\nAnd it works.", concatenate.execute());
   }
 }
