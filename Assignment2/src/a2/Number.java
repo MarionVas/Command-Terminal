f40538b5db859f9cafc6563ef4f29e3f218f6c0f
shahid41@UTORONTO.CA <shahid41@UTORONTO.CA@fb2d0955-5374-4267-af28-b32595e4e7c6>
2016-07-16 00:39:32 +0000

    Finished the !number command and updated for bugs

--- Assignment2/src/a2/Number.java
+++ Assignment2/src/a2/Number.java
@@ -2,57 +2,75 @@ package a2;
 
 import java.util.Vector;
 
-public class Number implements CommandInterface
-{
+public class Number implements CommandInterface {
   private String commandNumber;
   private ProQuery process;
   private JFileSystem fileManager;
   private History commandHistory;
   private String output = "";
-  
-  // Standard constructor for the number class
-  public Number(JFileSystem fileManager, ProQuery process, String number)
-  {
+  private static final int REPLACELOCATION = 3;
+
+  /**
+   * The constructor for the class
+   * @param fileManager
+   * @param process
+   * @param number
+   */
+  public Number(JFileSystem fileManager, ProQuery process, String number) {
+    // contains a proquery instance
     this.process = process;
+    // A string instance that holds the command to be re-executed
     this.commandNumber = number;
+    // Uses the given file system
     this.fileManager = fileManager;
   }
 
   @Override
-  public String execute() 
-  {
-    System.out.println("MADE IT TO CLASS");
+  public String execute() {
+    // Stores the command location
     int commandLocation = 0;
     // check if the parameter is a number
-    try
-    {
+    try {
       commandLocation = Integer.parseInt(commandNumber);
-    } catch (Exception e)
-    {
-      Output.printError();
+      if (commandLocation > 0) {
+        // because our vectors start from 0, reduce the number by one
+        commandLocation = commandLocation - 1;
+        // Get History from ProQuery
+        History commandHistory = process.getHistory();
+        Vector<String> allCommands = commandHistory.getList();
+
+
+        // Get the command at the specified user location
+        String query =
+            allCommands.get(commandLocation).substring(REPLACELOCATION);
+        
+        // remove the !number command from the history
+        commandHistory.popHistory();
+
+        // set the ProQuery history
+        process.setHistory(commandHistory);
+        // do the past command
+        process.sortQuery(query);
+      } else {
+        System.out.println("A number greater than 0 must be chosen");
+      }
+    } catch (NumberFormatException e) {
+      System.out.println("A number must be given");
     }
-    
-    // Get History from ProQuery
-    History commandHistory = process.getHistory();
-    Vector<String> allCommands = commandHistory.getList();
-    // Get the command at the specified user location
-    String query = allCommands.get(commandLocation);
-    // Remove the !number command from the history
-    commandHistory.popHistory();
-    // set the ProQuery history
-    process.setHistory(commandHistory);
-    // do the past command
-    process.sortQuery(query);
-    
-    return output;  
+    return output;
+
   }
 
+  /**
+   * Method returns the manual of how the !number command actually works
+   */
   @Override
   public String manual() {
-    // TODO Auto-generated method stub
-    return null;
+    return "!number - This command will recall any of previous history\n"
+        + "by its number(>=1) +preceded by an exclamation point and will then\n"
+        + "execute the command associated with that history number";
   }
-  
-  
+
+
 
 }
