88477e397692fe43e67a7e7c760dfa57fa67da5e
gaoxu1@UTORONTO.CA <gaoxu1@UTORONTO.CA@fb2d0955-5374-4267-af28-b32595e4e7c6>
2016-07-16 03:51:00 +0000

    "edited echo tests to only print the Strings, added redirection of output"

--- Assignment2/src/test/EchoTest.java
+++ Assignment2/src/test/EchoTest.java
@@ -11,6 +11,10 @@ public class EchoTest {
   private JFileSystem jFileSystem;
   private Echo echo;
   private String[] parameters;
+  private ProQuery runCommand;
+  private Cat concatenate;
+  private String[] strings = {"output.txt"};
+
 
   @Before
   public void setUp() throws Exception {
@@ -18,168 +22,125 @@ public class EchoTest {
     Folder rootFolder = new Folder("/", "/");
     jFileSystem.setRoot(rootFolder);
     jFileSystem.setCurrFolder(rootFolder);
+    this.runCommand = new ProQuery(this.jFileSystem);
+    this.concatenate = new Cat(this.jFileSystem, strings);
   }
 
   // print a string
   @Test
-  public void testExecute1() {
+  public void testEchoPrintSingleString() {
     /*
      * test to using Echo to print a string
      * 
-     * Expected type of echo command to be called is null.
      * 
      * Expected output string should match the String given in the parameters of
      * the constructor
      */
     parameters = new String[1];
-    parameters[0] = "\"This is a test.\"";
+    parameters[0] = "\"Passed.\"";
     echo = new Echo(jFileSystem, parameters);
-    echo.execute();
-    assertEquals(echo.getEchoType(), null);
-    assertEquals(echo.getStringToOutput(), "This is a test.");
+    assertEquals(echo.execute(), "Passed.");
   }
 
-  public void testExecute2() {
+  @Test
+  public void testEchoPrintMessageString() {
+    /*
+     * test to using Echo to print a message
+     * 
+     * 
+     * Expected output string should match the String given in the parameters of
+     * the constructor concatenated
+     */
+    parameters = new String[4];
+    parameters[0] = "\"This";
+    parameters[1] = "is";
+    parameters[2] = "a";
+    parameters[3] = "test.\"";
+    echo = new Echo(jFileSystem, parameters);
+    assertEquals(echo.execute(), "This is a test.");
+  }
+
+  public void testPrintEmptyString() {
     /*
      * test to using Echo to print an empty string
-     * 
-     * Expected type of echo command to be called is null.
      * 
      * Expected output string should be an empty String
      */
     parameters = new String[1];
     parameters[0] = "\"\"";
     echo = new Echo(jFileSystem, parameters);
-    echo.execute();
-    assertEquals(echo.getEchoType(), null);
-    assertEquals(echo.getStringToOutput(), "");
+    assertEquals(echo.execute(), "");
   }
 
   @Test
-  public void testExecute3() {
+  public void testPrintInvalidSingleString() {
     /*
-     * test to use Echo to call echoOverwrite class
-     * 
-     * Expected type of echo command to be called is overwrite.
-     */
-    parameters = new String[3];
-    parameters[0] = "\"test2 has passed.\"";
-    parameters[1] = ">";
-    parameters[2] = "test2";
-    echo = new Echo(jFileSystem, parameters);
-    echo.execute();
-    assertEquals(echo.getEchoType(), "overwrite");
-  }
-
-  @Test
-  public void testExecute4() {
-    /*
-     * test to use Echo to call echoAppend class
-     * 
-     * Expected type of echo command to be called is append.
-     */
-    parameters = new String[3];
-    parameters[0] = "\"test3 has passed.\"";
-    parameters[1] = ">>";
-    parameters[2] = "test3";
-    echo = new Echo(jFileSystem, parameters);
-    echo.execute();
-    assertEquals(echo.getEchoType(), "append");
-  }
-
-  @Test
-  public void testExecute5() {
-    /*
-     * test to use Echo to call echoOverwrite class with Strings with spaces for
-     * String parameter
-     * 
-     * Expected type of echo command to be called is overwrite.
-     */
-    parameters = new String[6];
-    parameters[0] = "\"test4";
-    parameters[1] = "has";
-    parameters[2] = "passed.\"";
-    parameters[3] = ">";
-    parameters[4] = "test4";
-    parameters[5] = ".";
-    echo = new Echo(jFileSystem, parameters);
-    echo.execute();
-    assertEquals(echo.getEchoType(), "overwrite");
-  }
-
-  @Test
-  public void testExecute6() {
-    /*
-     * test to use Echo to call echoAppend class with Strings with spaces for
-     * String parameter
-     * 
-     * Expected type of echo command to be called is append.
-     */
-    parameters = new String[6];
-    parameters[0] = "\"test4";
-    parameters[1] = "has";
-    parameters[2] = "passed.\"";
-    parameters[3] = ">>";
-    parameters[4] = "test4";
-    parameters[5] = ".";
-    echo = new Echo(jFileSystem, parameters);
-    echo.execute();
-    assertEquals(echo.getEchoType(), "append");
-  }
-
-  @Test
-  public void testExecute7() {
-    /*
-     * test to use Echo to call echoOverwrite class with Strings with spaces for
-     * String parameter
-     * 
-     * Expected type of echo command to be called is overwrite.
-     */
-    parameters = new String[3];
-    parameters[0] = "\"test5 has passed.\"";
-    parameters[1] = "&";
-    parameters[2] = "test5";
-    echo = new Echo(jFileSystem, parameters);
-    echo.execute();
-    assertEquals(echo.getEchoType(), null);
-    assertEquals(echo.getStringToOutput(), "");
-  }
-
-  @Test
-  public void testExecute8() {
-    /*
-     * test to use Echo to with invalid symbol as parameter
-     * 
-     * Expected type of echo command to be called is null.
+     * test to using Echo to print an invalid word
      * 
      * Expected output string should be an empty String
      */
-    parameters = new String[3];
-    parameters[0] = "\"test6 has passed.\"";
-    parameters[1] = "<";
-    parameters[2] = "test6";
+    parameters = new String[1];
+    parameters[0] = "\"Hi";
     echo = new Echo(jFileSystem, parameters);
-    echo.execute();
-    assertEquals(echo.getEchoType(), null);
-    assertEquals(echo.getStringToOutput(), "");
+    assertEquals(echo.execute(), "");
   }
 
   @Test
-  public void testExecute9() {
+  public void testPrintInvalidMessageString() {
     /*
-     * test to use Echo to with invalid symbol as parameter
-     * 
-     * Expected type of echo command to be called is null.
+     * test to using Echo to print an invalid message
      * 
      * Expected output string should be an empty String
      */
-    parameters = new String[3];
-    parameters[0] = "\"test7 has passed.\"";
-    parameters[1] = "<<";
-    parameters[2] = "test7";
+    parameters = new String[1];
+    parameters[0] = "This test has failed.\"";
     echo = new Echo(jFileSystem, parameters);
-    echo.execute();
-    assertEquals(echo.getEchoType(), null);
-    assertEquals(echo.getStringToOutput(), "");
+    assertEquals(echo.execute(), "");
+  }
+
+  @Test
+  public void testEchoRedirectOverwriteString() {
+    /*
+     * Test to determine if redirecting output of pwd command in new folder
+     * works. Creates a new file called path.txt, adds the output to the body of
+     * the new file.
+     * 
+     * Expected content of path.txt should be a string containing the path of
+     * the newly created working directory ("/a")
+     */
+    runCommand.sortQuery("echo \"This is a test.\" > output.txt");
+    concatenate = new Cat(runCommand.getFileSystem(), strings);
+    assertEquals("This is a test.", concatenate.execute());
+  }
+
+  @Test
+  public void testEchoRedirectAppendString() {
+    /*
+     * Test to determine if appending the output of pwd command in new folder to
+     * an existing path.txt file works. Creates a new file called path.txt, adds
+     * both outputs to the body of the new file.
+     * 
+     * Expected content of path.txt should be a string containing the path of
+     * the newly created working directory twice separated by a new line
+     * ("/a\n/a")
+     */
+    runCommand.sortQuery("echo \"This is a test.\" > output.txt");
+    runCommand.sortQuery("echo \"The test has passed.\" >> output.txt");
+    concatenate = new Cat(runCommand.getFileSystem(), strings);
+    assertEquals("This is a test.\nThe test has passed.",
+        concatenate.execute());
+  }
+
+  @Test
+  public void testEchoRedirectErrorString() {
+    /*
+     * Test to determine if redirecting an invalid String will not create a file
+     * 
+     * Expected no file called output.txt should be in the root folder of the
+     * jFileSystem
+     */
+    runCommand.sortQuery("echo \"This is a test. > output.txt");
+    Item file = jFileSystem.getObject("/output.txt");
+    assertTrue(file == null);
   }
 }
